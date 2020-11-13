d4167a70eb7023c4676e662fa7c4d4777c8a1478
turboLJY <lijunyi@ruc.edu.cn>
2020-11-13 02:31:56 +0000

    FEA: revise transfomer layer and add transformer encoder

--- textbox/module/layers.py
+++ textbox/module/layers.py
@@ -21,8 +21,9 @@ import copy
 import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as fn
+import torch.nn.functional as F
 from torch.nn.init import normal_
+from textbox.module.Attention.multi_head_attention import MultiHeadAttention
 from textbox.utils import ModelType, InputType, FeatureType
 
 
@@ -336,79 +337,6 @@ class VanillaAttention(nn.Module):
         return hidden_states, weights
 
 
-class MultiHeadAttention(nn.Module):
-    """
-    Multi-head Self-attention layers, a attention score dropout layer is introduced.
-
-    Args:
-        input_tensor (torch.Tensor): the input of the multi-head self-attention layer
-        attention_mask (torch.Tensor): the attention mask for input tensor
-
-    Returns:
-        hidden_states (torch.Tensor): the output of the multi-head self-attention layer
-
-    """
-    def __init__(self, n_heads, hidden_size, hidden_dropout_prob, attn_dropout_prob, layer_norm_eps):
-        super(MultiHeadAttention, self).__init__()
-        if hidden_size % n_heads != 0:
-            raise ValueError(
-                "The hidden size (%d) is not a multiple of the number of attention "
-                "heads (%d)" % (hidden_size, n_heads))
-
-        self.num_attention_heads = n_heads
-        self.attention_head_size = int(hidden_size / n_heads)
-        self.all_head_size = self.num_attention_heads * self.attention_head_size
-
-        self.query = nn.Linear(hidden_size, self.all_head_size)
-        self.key = nn.Linear(hidden_size, self.all_head_size)
-        self.value = nn.Linear(hidden_size, self.all_head_size)
-
-        self.attn_dropout = nn.Dropout(attn_dropout_prob)
-
-        self.dense = nn.Linear(hidden_size, hidden_size)
-        self.LayerNorm = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
-        self.out_dropout = nn.Dropout(hidden_dropout_prob)
-
-    def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
-        x = x.view(*new_x_shape)
-        return x.permute(0, 2, 1, 3)
-
-    def forward(self, input_tensor, attention_mask):
-        mixed_query_layer = self.query(input_tensor)
-        mixed_key_layer = self.key(input_tensor)
-        mixed_value_layer = self.value(input_tensor)
-
-        query_layer = self.transpose_for_scores(mixed_query_layer)
-        key_layer = self.transpose_for_scores(mixed_key_layer)
-        value_layer = self.transpose_for_scores(mixed_value_layer)
-
-        # Take the dot product between "query" and "key" to get the raw attention scores.
-        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-        # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
-        # [batch_size heads seq_len seq_len] scores
-        # [batch_size 1 1 seq_len]
-        attention_scores = attention_scores + attention_mask
-
-        # Normalize the attention scores to probabilities.
-        attention_probs = nn.Softmax(dim=-1)(attention_scores)
-        # This is actually dropping out entire tokens to attend to, which might
-        # seem a bit unusual, but is taken from the original Transformer paper.
-
-        attention_probs = self.attn_dropout(attention_probs)
-        context_layer = torch.matmul(attention_probs, value_layer)
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(*new_context_layer_shape)
-        hidden_states = self.dense(context_layer)
-        hidden_states = self.out_dropout(hidden_states)
-        hidden_states = self.LayerNorm(hidden_states + input_tensor)
-
-        return hidden_states
-
-
 class FeedForward(nn.Module):
     """
     Point-wise feed-forward layer is implemented by two dense layers.
@@ -420,24 +348,22 @@ class FeedForward(nn.Module):
         hidden_states (torch.Tensor): the output of the point-wise feed-forward layer
 
     """
-    def __init__(self, hidden_size, inner_size, hidden_dropout_prob, hidden_act, layer_norm_eps):
+    def __init__(self, embedding_size, inner_size, activate_function):
         super(FeedForward, self).__init__()
-        self.dense_1 = nn.Linear(hidden_size, inner_size)
-        self.intermediate_act_fn = self.get_hidden_act(hidden_act)
+        self.dense_1 = nn.Linear(embedding_size, inner_size)
+        self.dense_2 = nn.Linear(inner_size, embedding_size)
 
-        self.dense_2 = nn.Linear(inner_size, hidden_size)
-        self.LayerNorm = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
-        self.dropout = nn.Dropout(hidden_dropout_prob)
+        self.intermediate_act_func = self.get_mediate_act(activate_function)
 
-    def get_hidden_act(self, act):
-        ACT2FN = {
-            "gelu": self.gelu,
-            "relu": fn.relu,
-            "swish": self.swish,
-            "tanh": torch.tanh,
-            "sigmoid": torch.sigmoid,
+    def get_mediate_act(self, act):
+        ACT2FUNC = {
+            'gelu': self.gelu,
+            'relu': F.relu,
+            'swish': self.swish,
+            'tanh': torch.tanh,
+            'sigmoid': torch.sigmoid,
         }
-        return ACT2FN[act]
+        return ACT2FUNC[act]
 
     def gelu(self, x):
         """Implementation of the gelu activation function.
@@ -455,91 +381,79 @@ class FeedForward(nn.Module):
 
     def forward(self, input_tensor):
         hidden_states = self.dense_1(input_tensor)
-        hidden_states = self.intermediate_act_fn(hidden_states)
-
+        hidden_states = self.intermediate_act_func(hidden_states)
         hidden_states = self.dense_2(hidden_states)
-        hidden_states = self.dropout(hidden_states)
-        hidden_states = self.LayerNorm(hidden_states + input_tensor)
 
         return hidden_states
 
 
 class TransformerLayer(nn.Module):
     """
-    One transformer layer consists of a multi-head self-attention layer and a point-wise feed-forward layer.
+        One transformer layer :
+            a multi-head self-attention,
+            a external multi-head self-attention layer (only for conditional decoder) and
+            a point-wise feed-forward layer.
 
-    Args:
-        hidden_states (torch.Tensor): the input of the multi-head self-attention sublayer
-        attention_mask (torch.Tensor): the attention mask for the multi-head self-attention sublayer
-
-    Returns:
-        feedforward_output (torch.Tensor): the output of the point-wise feed-forward sublayer, is the output of the transformer layer
-
-    """
-    def __init__(self, n_heads, hidden_size, intermediate_size,
-                 hidden_dropout_prob, attn_dropout_prob, hidden_act, layer_norm_eps):
-        super(TransformerLayer, self).__init__()
-        self.multi_head_attention = MultiHeadAttention(n_heads, hidden_size,
-                                       hidden_dropout_prob, attn_dropout_prob, layer_norm_eps)
-        self.feed_forward = FeedForward(hidden_size, intermediate_size,
-                                         hidden_dropout_prob, hidden_act, layer_norm_eps)
-
-    def forward(self, hidden_states, attention_mask):
-        attention_output = self.multi_head_attention(hidden_states, attention_mask)
-        feedforward_output = self.feed_forward(attention_output)
-        return feedforward_output
-
-
-class TransformerEncoder(nn.Module):
-    r""" One TransformerEncoder consists of several TransformerLayers.
-
-        - n_layers(num): num of transformer layers in transformer encoder. Default: 2
-        - n_heads(num): num of attention heads for multi-head attention layer. Default: 2
-        - hidden_size(num): the input and output hidden size. Default: 64
-        - inner_size(num): the dimensionality in feed-forward layer. Default: 256
-        - hidden_dropout_prob(float): probability of an element to be zeroed. Default: 0.5
-        - attn_dropout_prob(float): probability of an attention score to be zeroed. Default: 0.5
-        - hidden_act(str): activation function in feed-forward layer. Default: 'gelu'
-                      candidates: 'gelu', 'relu', 'swish', 'tanh', 'sigmoid'
-        - layer_norm_eps(float): a value added to the denominator for numerical stability. Default: 1e-12
-
-    """
-    def __init__(self,
-                 n_layers=2,
-                 n_heads=2,
-                 hidden_size=64,
-                 inner_size=256,
-                 hidden_dropout_prob=0.5,
-                 attn_dropout_prob=0.5,
-                 hidden_act='gelu',
-                 layer_norm_eps=1e-12):
-
-        super(TransformerEncoder, self).__init__()
-        layer = TransformerLayer(n_heads, hidden_size, inner_size,
-                                 hidden_dropout_prob, attn_dropout_prob, hidden_act, layer_norm_eps)
-        self.layer = nn.ModuleList([copy.deepcopy(layer)
-                                    for _ in range(n_layers)])
-
-    def forward(self, hidden_states, attention_mask, output_all_encoded_layers=True):
-        """
         Args:
-            hidden_states (torch.Tensor): the input of the TrandformerEncoder
-            attention_mask (torch.Tensor): the attention mask for the input hidden_states
-            output_all_encoded_layers (Bool): whether output all transformer layers' output
-
+            self_padding_mask (torch.bool): the padding mask for the multi head attention sublayer.
+            self_attn_mask (torch.bool): the attention mask for the multi head attention sublayer.
+            external_states (torch.Tensor): the external context for decoder, e.g., hidden states from encoder.
+            external_padding_mask (torch.bool): the padding mask for the external states.
         Returns:
-            all_encoder_layers (list): if output_all_encoded_layers is True, return a list consists of all transformer layers' output,
-            otherwise return a list only consists of the output of last transformer layer.
-
+            feedforward_output (torch.Tensor): the output of the point-wise feed-forward sublayer, is the output of the transformer layer
         """
-        all_encoder_layers = []
-        for layer_module in self.layer:
-            hidden_states = layer_module(hidden_states, attention_mask)
-            if output_all_encoded_layers:
-                all_encoder_layers.append(hidden_states)
-        if not output_all_encoded_layers:
-            all_encoder_layers.append(hidden_states)
-        return all_encoder_layers
+    def __init__(self, embedding_size, ffn_size, num_heads, attn_dropout=0.0, attn_weight_dropout=0.0,
+                 ffn_dropout=0.0, ffn_activate_func='gelu', with_external=False):
+        super(TransformerLayer, self).__init__()
+        self.multi_head_attention = MultiHeadAttention(embedding_size, num_heads, attn_weight_dropout)
+        self.feed_forward = FeedForward(embedding_size, ffn_size, ffn_activate_func)
+
+        self.attn_layer_norm = nn.LayerNorm(embedding_size)
+        self.ffn_layer_norm = nn.LayerNorm(embedding_size)
+
+        self.attn_dropout = nn.Dropout(attn_dropout)
+        self.ffn_dropout = nn.Dropout(ffn_dropout)
+
+        self.with_external = with_external
+
+        if self.with_external:
+            self.external_multi_head_attention = MultiHeadAttention(embedding_size, num_heads, attn_weight_dropout)
+            self.external_layer_norm = nn.LayerNorm(embedding_size)
+
+    def forward(self, x, kv=None,
+                self_padding_mask=None, self_attn_mask=None,
+                external_states=None, external_padding_mask=None):
+        residual = x
+        if kv is None:
+            # Usually for Transformer encoder, key_padding_mask=attn_mask
+            x, self_attn_weights = self.multi_head_attention(query=x, key=x, value=x,
+                                                             key_padding_mask=self_padding_mask,
+                                                             attn_mask=self_attn_mask)
+        else:
+            # Usually for Transformer decoder, attn_mask is a a lower triangular matrix
+            x, self_attn_weights = self.multi_head_attention(query=x, key=kv, value=kv,
+                                                             key_padding_mask=self_padding_mask,
+                                                             attn_mask=self_attn_mask)
+        x = self.attn_dropout(x)
+        x = self.attn_layer_norm(residual + x)
+
+        if self.with_external:
+            # Usually for conditional Transformer decoder
+            residual = x
+            x, external_attn_weights = self.external_multi_head_attention(query=x, key=external_states,
+                                                                          value=external_memories,
+                                                                          key_padding_mask=external_padding_mask)
+            x = self.attn_dropout(x)
+            x = self.external_layer_norm(residual + x)
+        else:
+            external_attn_weights = None
+
+        residual = x
+        x = self.feed_forward(x)
+        x = self.ffn_dropout(x)
+        x = self.ffn_layer_norm(residual + x)
+
+        return x, self_attn_weights, external_attn_weights
 
 
 class ContextSeqEmbAbstractLayer(nn.Module):

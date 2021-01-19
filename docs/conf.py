7460d79a80d9a5cd32799d4c5699833085201bd6
StevenTang1998 <1020139164@qq.com>
2021-01-19 21:31:06 +0800

    FEA: new version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, AI Box'
 author = 'AI Box'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.6'
 
 
 # -- General configuration ---------------------------------------------------

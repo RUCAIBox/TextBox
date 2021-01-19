3b0a9260b0d73df5345cb36620d4d398b4a330c9
StevenTang1998 <1020139164@qq.com>
2021-01-19 20:40:59 +0800

    Format

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ import os
 
 from setuptools import setup, find_packages
 
-install_requires = ['matplotlib>=3.1.3', 'torch>=1.6.0', 'numpy>=1.17.2', 'nltk>=3.4.5',
-                    'pyyaml>=5.3.1', 'fast_bleu>=0.0.86', 'py-rouge>=1.1','transformers>=4.0.1']
+install_requires = [
+    'matplotlib>=3.1.3', 'torch>=1.6.0', 'numpy>=1.17.2', 'nltk>=3.4.5', 'pyyaml>=5.3.1', 'fast_bleu>=0.0.86',
+    'py-rouge>=1.1', 'transformers>=4.0.1'
+]
 
 setup_requires = []
 
@@ -17,12 +19,11 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     install_requires.extend(setup_requires)
 
-classifiers = ['License :: OSI Approved :: MIT License',
-               'Operating System :: OS Independent',
-               'Programming Language :: Python :: 3',
-               'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7',
-               'Programming Language :: Python :: 3.8']
+classifiers = [
+    'License :: OSI Approved :: MIT License', 'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.8'
+]
 
 with open("PYPI.md", "r") as f:
     long_description = f.read()
@@ -37,12 +38,10 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.6.2",
-    packages=[
-        package for package in find_packages()
-        if package.startswith('textbox')
-    ],
+    packages=[package for package in find_packages() if package.startswith('textbox')],
     include_package_data=True,
     install_requires=install_requires,
     setup_requires=setup_requires,
     extras_require=extras_require,
-    zip_safe=False)
+    zip_safe=False
+)

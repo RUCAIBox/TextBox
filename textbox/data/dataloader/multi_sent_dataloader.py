7e16418a5ef94c77d02ae76ca2e5e00e12105e92
StevenTang1998 <1020139164@qq.com>
2021-02-05 10:46:49 +0800

    Refactor: a unified dataset/dataloader

--- textbox/data/dataloader/multi_sent_dataloader.py
+++ textbox/data/dataloader/multi_sent_dataloader.py
@@ -19,13 +19,14 @@ class MultipleSentenceDataLoader(AbstractDataLoader):
 
     Args:
         config (Config): The config of dataloader.
-        dataset (SingleSentenceDataset): The dataset of dataloader. Corpus, see textbox.data.corpus for more details
+        dataset (MultipleSentenceDataset): The dataset of dataloader. Corpus, see textbox.data.corpus for more details
         batch_size (int, optional): The batch_size of dataloader. Defaults to ``1``.
         shuffle (bool, optional): Whether the dataloader will be shuffle after a round. Defaults to ``False``.
     """
 
     def __init__(self, config, dataset, batch_size=1, shuffle=False):
         super().__init__(config, dataset, batch_size=batch_size, shuffle=shuffle)
+        self.group_data_key = []
         self.data_preprocess(dataset)
 
     def _build_multi_data(self, text_data, token2idx, need_text_start_end=True):
@@ -36,42 +37,46 @@ class MultipleSentenceDataLoader(AbstractDataLoader):
             need_text_start_end, bool, indicates whether we should add sos and eos token index.
         output:
             text_idx_data: list -> list -> list -> int, list of word index
-            text_idx_length_data: list -> list -> int, list of sequence length
-            sent_num_data: list of sentence number
+            idx_length_data: list -> list -> int, list of sequence length
+            idx_num_data: list of sentence number
         """
         text_idx_data = []
-        text_idx_length_data = []
-        sent_num_data = []
-        for sens in text_data:
-            sens_idx_data = []
+        idx_length_data = []
+        idx_num_data = []
+        for text in text_data:
+            idx_data = []
             idx_length = []
-            for sen in sens:
-                text_idx = self._token2idx(sen, token2idx)
+            for sent in text:
+                text_idx = self._token2idx(sent, token2idx)
                 if need_text_start_end:
                     text_idx = [self.sos_token_idx] + text_idx + [self.eos_token_idx]
-                sens_idx_data.append(text_idx)
+                idx_data.append(text_idx)
                 idx_length.append(len(text_idx))
-            text_idx_data.append(sens_idx_data)
-            text_idx_length_data.append(idx_length)
-            sent_num_data.append(len(sens_idx_data))
-        return text_idx_data, text_idx_length_data, sent_num_data
+            text_idx_data.append(idx_data)
+            idx_length_data.append(idx_length)
+            idx_num_data.append(len(text))
+        return text_idx_data, idx_length_data, idx_num_data
 
     def data_preprocess(self, dataset):
-        required_key_list = [
-            'idx2token', 'token2idx', 'knowledge_text_data', 'source_text_data', 'target_text_data'
-        ]
+        required_key_list = ['idx2token', 'token2idx']
         for dataset_attr in required_key_list:
             assert dataset_attr in dataset
             setattr(self, dataset_attr, dataset[dataset_attr])
-        self.knowledge_text_idx_data, self.knowledge_idx_length_data, self.knowledge_sent_num_data = self._build_multi_data(
-            self.knowledge_text_data, self.token2idx
-        )
-        self.source_text_idx_data, self.source_idx_length_data, self.source_sent_num_data = self._build_multi_data(
-            self.source_text_data, self.token2idx
-        )
-        self.target_text_idx_data, self.target_idx_length_data = self._build_data(
-            self.target_text_data, self.token2idx
-        )
+        for group in ['knowledge', 'source', 'target']:
+            text_name = group + '_text_data'
+            if text_name in dataset:
+                text_data = dataset[text_name]
+                setattr(self, text_name, text_data)
+                if isinstance(text_data[0][0], str):
+                    text_idx, idx_length = self._build_data(text_data, self.token2idx)
+                else:
+                    text_idx, idx_length, idx_num = self._build_multi_data(text_data, self.token2idx)
+                    setattr(self, group + '_idx_num_data', idx_num)
+                    self.group_data_key.append(group + '_idx_num_data')
+                setattr(self, group + '_text_idx_data', text_idx)
+                self.group_data_key.append(group + '_text_idx_data')
+                setattr(self, group + '_idx_length_data', idx_length)
+                self.group_data_key.append(group + '_idx_length_data')
 
     def get_reference(self):
         return self.target_text_data
@@ -84,68 +89,57 @@ class MultipleSentenceDataLoader(AbstractDataLoader):
         return math.ceil(len(self.target_text_idx_data) / self.batch_size)
 
     def _shuffle(self):
-        temp = list(
-            zip(
-                self.knowledge_text_idx_data, self.knowledge_idx_length_data, self.knowledge_sent_num_data,
-                self.source_text_idx_data, self.source_idx_length_data, self.source_sent_num_data,
-                self.target_text_idx_data, self.target_idx_length_data
-            )
-        )
+        data_list = []
+        for key in self.group_data_key:
+            data_list.append(getattr(self, key))
+        temp = list(zip(*data_list))
         random.shuffle(temp)
-        self.knowledge_text_idx_data[:], self.knowledge_idx_length_data[:], self.knowledge_sent_num_data[:], \
-        self.source_text_idx_data[:], self.source_idx_length_data[:], self.source_sent_num_data[:], \
-        self.target_text_idx_data[:], self.target_idx_length_data[:] = zip(*temp)
+        temp = list(zip(*temp))
+        for i, key in enumerate(self.group_data_key):
+            setattr(self, key, list(temp[i]))
 
-    def _pad_batch_multi_sequence(self, text_idx_data, idx_length_data, sentence_num):
-        max_sentence_num = max(sentence_num)
-        max_length = max([max(sent_len) for sent_len in idx_length_data])
+    def _pad_batch_multi_sequence(self, text_idx_data, idx_length_data, idx_num_data):
+        max_num = max(idx_num_data)
+        max_length = max([max(length) for length in idx_length_data])
 
         new_length_data = []
-        new_data = []
-        for (text_idx, idx_len, sent_num) in zip(text_idx_data, idx_length_data, sentence_num):
-            new_length_data.append(idx_len + [0] * (max_sentence_num - sent_num))
+        new_idx_data = []
+        for (text_idx, idx_len, idx_num) in zip(text_idx_data, idx_length_data, idx_num_data):
+            new_length_data.append(idx_len + [0] * (max_num - idx_num))
             new_sent_data = []
             for (sent_idx, idx_len) in zip(text_idx, idx_len):
                 new_sent_data.append(sent_idx + [self.padding_token_idx] * (max_length - idx_len))
-            for _ in range(max_sentence_num - sent_num):
+            for _ in range(max_num - idx_num):
                 new_sent_data.append([0] * max_length)
-            new_data.append(new_sent_data)
-        sentence_num = torch.LongTensor(sentence_num)
+            new_idx_data.append(new_sent_data)
+        new_num_data = torch.LongTensor(idx_num_data)
         new_length_data = torch.LongTensor(new_length_data)
-        new_data = torch.LongTensor(new_data)
-        return new_data, new_length_data, sentence_num
-        
+        new_idx_data = torch.LongTensor(new_idx_data)
+        return new_idx_data, new_length_data, new_num_data
+
     def _next_batch_data(self):
-        knowledge_text = self.knowledge_text_data[self.pr:self.pr + self.step]
-        tp_knowledge_text_idx_data = self.knowledge_text_idx_data[self.pr:self.pr + self.step]
-        tp_knowledge_idx_length_data = self.knowledge_idx_length_data[self.pr:self.pr + self.step]
-        tp_knowledge_sent_num = self.knowledge_sent_num_data[self.pr:self.pr + self.step]
-        knowledge_idx, knowledge_length, knowledge_sentence_num = self._pad_batch_multi_sequence(tp_knowledge_text_idx_data, tp_knowledge_idx_length_data, tp_knowledge_sent_num)
+        batch_data = {}
+        for group in ['knowledge', 'source', 'target']:
+            text_name = group + '_text_data'
+            if hasattr(self, text_name):
+                batch_data[text_name] = getattr(self, text_name)[self.pr:self.pr + self.step]
 
-        source_text = self.source_text_data[self.pr:self.pr + self.step]
-        tp_source_text_idx_data = self.source_text_idx_data[self.pr:self.pr + self.step]
-        tp_source_idx_length_data = self.source_idx_length_data[self.pr:self.pr + self.step]
-        tp_source_sent_num = self.source_sent_num_data[self.pr:self.pr + self.step]
-        source_idx, source_length, source_sentence_num = self._pad_batch_multi_sequence(tp_source_text_idx_data, tp_source_idx_length_data, tp_source_sent_num)
+                idx_name = group + '_text_idx_data'
+                length_name = group + '_idx_length_data'
+                num_name = group + '_idx_num_data'
+                tp_text_idx = getattr(self, idx_name)[self.pr:self.pr + self.step]
+                tp_idx_length = getattr(self, length_name)[self.pr:self.pr + self.step]
+                if hasattr(self, num_name):
+                    tp_idx_num = getattr(self, num_name)[self.pr:self.pr + self.step]
+                    text_idx, idx_length, idx_num = self._pad_batch_multi_sequence(
+                        tp_text_idx, tp_idx_length, tp_idx_num
+                    )
+                    batch_data[num_name] = idx_num.to(self.device)
+                else:
+                    text_idx, idx_length = self._pad_batch_sequence(tp_text_idx, tp_idx_length)
 
-        target_text = self.target_text_data[self.pr:self.pr + self.step]
-        tp_target_text_idx_data = self.target_text_idx_data[self.pr:self.pr + self.step]
-        tp_target_idx_length_data = self.target_idx_length_data[self.pr:self.pr + self.step]
-        target_idx, target_length = self._pad_batch_sequence(tp_target_text_idx_data, tp_target_idx_length_data)
+                batch_data[idx_name] = text_idx.to(self.device)
+                batch_data[length_name] = idx_length.to(self.device)
 
         self.pr += self.step
-
-        batch_data = {
-            'knowledge_text': knowledge_text,
-            'knowledge_idx': knowledge_idx.to(self.device),
-            'knowledge_sentence_num': knowledge_sentence_num.to(self.device),
-            'knowledge_length': knowledge_length.to(self.device),
-            'source_text': source_text,
-            'source_idx': source_idx.to(self.device),
-            'source_sentence_num': source_sentence_num.to(self.device),
-            'source_length': source_length.to(self.device),
-            'target_text': target_text,
-            'target_idx': target_idx.to(self.device),
-            'target_length': target_length.to(self.device)
-        }
         return batch_data

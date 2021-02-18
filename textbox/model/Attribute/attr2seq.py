3013589868f051165bf2f3667114e6664b84d392
Timothy023 <602525931@qq.com>
2021-02-18 16:10:56 +0800

    attr2seq.py 2.0

--- textbox/model/Attribute/attr2seq.py
+++ textbox/model/Attribute/attr2seq.py
@@ -64,34 +64,25 @@ class Attr2Seq(AttributeGenerator):
             self.dropout_ratio, self.attention_type, self.alignment_method
         )
 
-        self.batch_size = config['train_batch_size']
-        self.device = config['device']
-
         self.dropout = nn.Dropout(self.dropout_ratio)
         self.vocab_linear = nn.Linear(self.hidden_size, self.vocab_size)
         self.loss = nn.CrossEntropyLoss(ignore_index=self.padding_token_idx, reduction='none')
 
-        self.max_target_length = config['target_max_seq_length']
+        self.max_target_length = config['max_seq_length']
 
-        # parameters initialization
-        self.apply(xavier_normal_initialization)
-
-        for i in range (self.attribute_num):
-            self.attribute_size[i] = self.attribute_size[i] + 1
-            
-        self.w = nn.ModuleList(
-            [nn.Linear(self.attribute_size[i], self.embedding_size) for i in range (self.attribute_num)]
-        )
+        self.w = nn.ModuleList([nn.Embedding(self.attribute_size[i], self.embedding_size) for i in range (self.attribute_num)])
 
         self.H = nn.Parameter(torch.rand((self.num_dec_layers * self.hidden_size, self.attribute_num * self.embedding_size), requires_grad=True)).to(self.device)
         self.b = nn.Parameter(torch.rand((self.num_dec_layers * self.hidden_size, 1), requires_grad=True)).to(self.device)
         self.c = nn.Parameter(torch.zeros((self.num_dec_layers, 1, self.hidden_size), requires_grad=True)).to(self.device)
 
+        # parameters initialization
+        self.apply(xavier_normal_initialization)
 
     def encoder(self, source_idx, source_length):
         r""" 
             Args:
-                source_idx (Torch.Tensor): source attribute index, shape: [source_length, attribute_size].
+                source_idx (Torch.Tensor): source attribute index, shape: [source_length, attribute_num].
                 source_length (integer): size of source.
             
             Returns:
@@ -99,40 +90,22 @@ class Attr2Seq(AttributeGenerator):
                     - Torch.Tensor: output features, shape: [source_length, attribute_num, embeding_size].
                     - Torch.Tensor: hidden states, shape: [source_length, num_dec_layers, hidden_size].
         """
-    
-        # source_idx[i]: shape[source_length, attribute_size[i]].
-        # w[i]: shape[attribute_size[i], embedding_size].
-        # g[i]: shape[embedding_size, source_length].
-        self.g = [torch.zeros((self.embedding_size, source_length), requires_grad=True).to(self.device) for i in range (self.attribute_num)]
-        for i in range (self.attribute_num):
-            # print("size of w[i]: ", self.w[i].size())
-            # print("size of source_idx[i]: ", source_idx[i].size())
-            # print("size of source_idx[i] after transpose: ", (source_idx[i].transpose(0, 1)).size())
-            # self.g[i] = torch.mm(self.w[i], source_idx[i].transpose(0, 1))
-            self.g[i] = self.w[i](source_idx[i]).transpose(0, 1)
-
-        # change the shape of g[], save in g1. New shape; [source_length, attribute_num * embedding_size, 1].
-        g1 = torch.zeros(source_length, self.attribute_num * self.embedding_size, 1).to(self.device)
-        for i in range (source_length):
-            for j in range (self.attribute_num):
-                g1[i, (j * self.embedding_size) : ((j + 1) * self.embedding_size), :] = self.g[j][:, i].unsqueeze(1)
+        # g1 (torch.Tensor): [source_length, attribute_num * embedding_size, 1].
+        g1 = self.w[0](source_idx[:, 0])
+        for i in range (1, self.attribute_num):
+            g1 = torch.cat((g1, self.w[i](source_idx[:, i])), 1)
+        g1 = g1.unsqueeze(2)
         
         #outputs (Torch.Tensor): shape: [source_length, attribute_num, embedding_size].
         outputs = g1.contiguous().view(source_length, self.attribute_num, self.embedding_size)
         
         # H  (Torch.Tensor): shape: [num_dec_layers * hidden_size, embedding_size * attribute_num].
         # H1 (Torch.Tensor): shape: [source_length, num_dec_layers * hidden_size, embedding_size * attribute_num].
-        tmp = self.H.unsqueeze(0)
-        H1 = tmp
-        for i in range (source_length - 1):
-            H1 = torch.cat((H1, tmp), 0)
+        H1 = self.H.unsqueeze(0).repeat(source_length, 1, 1)
 
         # b  (Torch.Tensor): shape: [num_dec_layers * hidden_size, 1].
         # b1 (Torch.Tensor): shape: [source_length, num_dec_layers * hidden_size, 1].
-        tmp = self.b.unsqueeze(0)
-        b1 = tmp
-        for i in range (source_length - 1):
-            b1 = torch.cat((b1, tmp), 0)
+        b1 = self.b.unsqueeze(0).repeat(source_length, 1, 1)
 
         # b1 (Torch.Tensor): shape: [source_length, num_dec_layers * hidden_size, 1].
         # g1 (Torch.Tensor): shape: [source_length, attribute_num * embedding_size, 1].
@@ -161,17 +134,11 @@ class Attr2Seq(AttributeGenerator):
             num += 1
             print('source_size: ', source_size, ';', 'num: ', num)
 
-            source_embeddings = [torch.zeros(source_size, self.attribute_size[i]).to(self.device) for i in range(self.attribute_num)]
-            for i in range (self.attribute_num):
-                for id in range (source_size):
-                    source_embeddings[i][id, source_idx[id][i]] = 1
-            
-            encoder_outputs, encoder_states = self.encoder(source_embeddings, source_size)
+            encoder_outputs, encoder_states = self.encoder(source_idx, source_size)
             encoder_masks = torch.ne(source_idx, self.padding_token_idx)
             
             for bid in range(source_size):
                 decoder_states = (encoder_states[:, bid, :].unsqueeze(1), self.c)
-                # decoder_states = encoder_states[:, bid, :].unsqueeze(1)
                 encoder_output = encoder_outputs[bid, :, :].unsqueeze(0)
                 encoder_mask = encoder_masks[bid, :].unsqueeze(0)
                 generate_tokens = []
@@ -182,7 +149,6 @@ class Attr2Seq(AttributeGenerator):
                         self.beam_size, self.sos_token_idx, self.eos_token_idx, self.device, idx2token
                     )
 
-                self.max_target_length = 200
                 for gen_idx in range(self.max_target_length):
                     decoder_input = self.target_token_embedder(input_seq)
                     decoder_outputs, decoder_states, _ = self.decoder(
@@ -195,9 +161,8 @@ class Attr2Seq(AttributeGenerator):
                     elif (self.strategy == 'greedy_search'):
                         token_idx = greedy_search(token_logits).item()
                     elif (self.strategy == 'beam_search'):
-                        input_seq, tmp_decoder_states, encoder_output, encoder_mask = \
+                        input_seq, decoder_states, encoder_output, encoder_mask = \
                             hypothesis.step(gen_idx, token_logits, decoder_states, encoder_output, encoder_mask)
-                        # decoder_states = (tmp_decoder_states, decoder_states[1])
 
                     if (self.strategy in ['topk_sampling', 'greedy_search']):
                         if token_idx == self.eos_token_idx:
@@ -220,42 +185,32 @@ class Attr2Seq(AttributeGenerator):
         self.sum = self.sum + 1
         print('in calculate_loss: ', self.sum, '.')
 
+        # target_length (Torch.Tensor): shape: [batch_size]
         target_length = corpus['target_length']
-        # attribute_idx (Torch.Tensor): shape: [source_length, attribute_num].
+        # attribute_idx (Torch.Tensor): shape: [batch_size, attribute_num].
         attribute_idx = corpus['attribute_idx']
-        source_length = attribute_idx.size(0)
-        # target_idx (torch.Tensor): shape: [source_length, target_length].
+        # target_idx (torch.Tensor): shape: [batch_size, length].
         target_idx = corpus['target_idx']
+        source_length = attribute_idx.size(0)
 
         print ('batch_size: ', self.batch_size, '; ', 'source_length: ', source_length, '; ', 'attribute_num: ', self.attribute_num, '; ', 'attribute_size: ', self.attribute_size[0], ', ', self.attribute_size[1], ', ',self.attribute_size[2])
-        #source_idx[i] (Torch.Tensor): shape: [source_length, attribute_size[i]]
-        source_idx = [torch.zeros(source_length, self.attribute_size[i]).to(self.device) for i in range(self.attribute_num)]
-        for i in range (self.attribute_num):
-            for id in range (source_length):
-                if (attribute_idx[id][i] >= self.attribute_size[i]):
-                    print ('attribute_idx error.')
-                source_idx[i][id, attribute_idx[id][i]] = 1
 
-        encoder_outputs, encoder_states = self.encoder(source_idx, source_length)
+        encoder_outputs, encoder_states = self.encoder(attribute_idx, source_length)
 
         input_text = target_idx[:, :-1]
         target_text = target_idx[:, 1:]
         input_embeddings = self.dropout(self.target_token_embedder(input_text))
         
-        # print("size of input_embeddings: ", input_embeddings.size())
-        # print("size of encoder_states: ", encoder_states.size())
-        # print("size of encoder_outputs: ", encoder_outputs.size())
-        c1 = self.c
-        for i in range (source_length - 1):
-            c1 = torch.cat((c1, self.c), 1)
+        c1 = self.c.repeat(1, source_length, 1)
         encoder_masks = torch.ne(attribute_idx, self.padding_token_idx)
-        decoder_outputs, decoder_states, _ = self.decoder(input_embeddings, (encoder_states.contiguous(), c1), encoder_outputs, encoder_masks)
+        decoder_outputs, decoder_states, _ = \
+            self.decoder(input_embeddings, (encoder_states.contiguous(), c1), encoder_outputs, encoder_masks)
 
-        # token_logits (Torch.Tensor): shape: [source_length, target_length, vocabulary_size].
+        # token_logits (Torch.Tensor): shape: [batch_size, target_length, vocabulary_size].
         token_logits = self.vocab_linear(decoder_outputs)
 
-        # token_logits.view(-1, token_logits.size(-1)) (Torch.Tensor): shape: [source_length * target_length, vocabulary_size].
-        # target_text.contiguous().view(-1) (Torch.Tensor): shape: [source_length * target_length].
+        # token_logits.view(-1, token_logits.size(-1)) (Torch.Tensor): shape: [batch_size * target_length, vocabulary_size].
+        # target_text.contiguous().view(-1) (Torch.Tensor): shape: [batch_size * target_length].
         loss = self.loss(token_logits.view(-1, token_logits.size(-1)), target_text.contiguous().view(-1))
         loss = loss.reshape_as(target_text)
 

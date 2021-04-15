47a1a3dc1eee71dd585124dbf1786e2508d34561
StevenTang1998 <1020139164@qq.com>
2021-04-15 17:30:10 +0800

    Refactor: optimize PLMs

--- textbox/model/Seq2Seq/bart.py
+++ textbox/model/Seq2Seq/bart.py
@@ -32,60 +32,45 @@ class BART(Seq2SeqGenerator):
         self.tokenizer = BartTokenizer.from_pretrained(self.pretrained_model_path, add_prefix_space=True)
         self.configuration = BartConfig.from_pretrained(self.pretrained_model_path)
 
-        self.decoder = BartForConditionalGeneration.from_pretrained(
-            self.pretrained_model_path, config=self.configuration
-        )
+        self.model = BartForConditionalGeneration.from_pretrained(self.pretrained_model_path, config=self.configuration)
 
         self.padding_token_idx = self.tokenizer.pad_token_id
         self.loss = nn.CrossEntropyLoss(ignore_index=self.padding_token_idx, reduction='none')
 
-    def generate(self, batch_data, eval_dataloader):
-        generate_corpus = []
-        source_text = batch_data["source_text"]
-        for text in source_text:
-            sentence = ' '.join(text)
-            encoding_dict = self.tokenizer(sentence, return_tensors="pt")
-            input_ids = encoding_dict['input_ids'].to(self.device)
-            sample_outputs = self.decoder.generate(
-                input_ids, num_beams=5, max_length=self.max_target_length, early_stopping=True
-            )
-            generated_text = self.tokenizer.decode(sample_outputs[0], skip_special_tokens=True)
-            generate_corpus.append(generated_text.lower().split())
+    def generate(self, batch_data, eval_data):
+        source_text = batch_data['source_text']
+        input_ids, attn_masks = self.tokenize_text(source_text)
+
+        sample_outputs = self.model.generate(
+            input_ids, attention_mask=attn_masks, num_beams=5, max_length=self.max_target_length, early_stopping=True
+        )
+        generated_text = self.tokenizer.batch_decode(sample_outputs, skip_special_tokens=True)
+        generate_corpus = [text.lower().split() for text in generated_text]
         return generate_corpus
+
+    def tokenize_text(self, text):
+        texts = [' '.join(t) for t in text]
+        encoding_dict = self.tokenizer(
+            texts, max_length=self.max_source_length, padding=True, truncation=True, return_tensors="pt"
+        )
+
+        input_ids = encoding_dict['input_ids'].to(self.device)
+        attn_masks = encoding_dict['attention_mask'].to(self.device)
+
+        return input_ids, attn_masks
 
     def forward(self, corpus, epoch_idx=-1):
         source_text = corpus['source_text']
         target_text = corpus['target_text']
 
-        input_ids = []
-        attn_masks = []
-        for text in source_text:
-            sentence = ' '.join(text)
-            encoding_dict = self.tokenizer(
-                sentence, max_length=self.max_source_length, padding="max_length", truncation=True, return_tensors="pt"
-            )
-            input_ids.append(encoding_dict['input_ids'])
-            attn_masks.append(encoding_dict['attention_mask'])
-        input_ids = torch.cat(input_ids, dim=0).to(self.device)
-        attn_masks = torch.cat(attn_masks, dim=0).to(self.device)
-
-        target_ids = []
-        decoder_attn_masks = []
-        for text in target_text:
-            sentence = ' '.join(text)
-            decoding_dict = self.tokenizer(
-                sentence, max_length=self.max_target_length, padding="max_length", truncation=True, return_tensors="pt"
-            )
-            target_ids.append(decoding_dict['input_ids'])
-            decoder_attn_masks.append(decoding_dict['attention_mask'])
-        target_ids = torch.cat(target_ids, dim=0).to(self.device)
-        decoder_attn_masks = torch.cat(decoder_attn_masks, dim=0).to(self.device)
+        input_ids, attn_masks = self.tokenize_text(source_text)
+        target_ids, decoder_attn_masks = self.tokenize_text(target_text)
 
         decoder_input_ids = target_ids[:, :-1].contiguous()
         decoder_attn_masks = decoder_attn_masks[:, :-1].contiguous()
         decoder_target_ids = target_ids[:, 1:].contiguous()
 
-        outputs = self.decoder(
+        outputs = self.model(
             input_ids,
             attention_mask=attn_masks,
             decoder_input_ids=decoder_input_ids,
@@ -98,6 +83,6 @@ class BART(Seq2SeqGenerator):
         loss = loss.reshape_as(decoder_target_ids)
 
         length = (decoder_target_ids != self.padding_token_idx).sum(dim=1).float()
-        loss = loss.sum(dim=1) / length.float()
+        loss = loss.sum(dim=1) / length
 
         return loss.mean()

5dc99e9cf78f7e9133121efe81f20491e9c163e9
leoleojie <88081427+leoleojie@users.noreply.github.com>
2021-12-17 11:43:56 +0800

    Create ModelConstructor.py
    Some modules of the table2text model are stored in this file
    such as Decoder, Encoder, Embeddings, BeamSearch, Attention,TextGeneration module

--- textbox/module/ModelConstructor.py
+++ textbox/module/ModelConstructor.py
@@ -5,8 +5,8 @@
 
 """
 textbox.module.ModelConstructor
-make model for table2text
-Contains Decoder, Encoder, Embeddings, BeamSearch, TextGeneration module 
+Some modules of the table2text model are stored in this file
+Contains Decoder, Encoder, Embeddings, BeamSearch, Attention,TextGeneration module 
 """
 from __future__ import division, unicode_literals
 import torch
@@ -16,7 +16,7 @@ from textbox.module.strategy import CopyGenerator
 from textbox.utils.enum_type import SpecialTokens
 from torch.nn.utils.rnn import pack_padded_sequence as pack
 from torch.nn.utils.rnn import pad_packed_sequence as unpack
-from textbox.module.Attention.PointerAttention import GlobalSelfAttention,GlobalAttention
+
 
 
 def make_embeddings(config, vocab_size, feat_vocab_size,word_padding_idx):
@@ -544,7 +544,7 @@ class RNNDecoderState(DecoderState):
         return self.hidden + (self.input_feed,)
 
     def update_state(self, rnnstate, input_feed, coverage):
-        if not isinstance(rnnstate, tuple): # 用最后一个时间步的hidden_state,更新 self.hidden
+        if not isinstance(rnnstate, tuple): 
             self.hidden = (rnnstate,)
         else:
             self.hidden = rnnstate
@@ -759,15 +759,9 @@ class Translator(object):
         batch_size = self.batch_size
         data_type = 'text'
         token2idx = data.target_token2idx
-        beam = [Beam(beam_size, n_best=self.n_best,
-                                    cuda=self.cuda,
-                                    global_scorer=self.global_scorer,
-                                    pad=self.pad,
-                                    eos=self.eos,
-                                    bos=self.sos,
-                                    min_length=self.min_length,
-                                    stepwise_penalty=self.stepwise_penalty)
-                for __ in range(batch_size)]
+        beam = [Beam(beam_size, n_best=self.n_best, cuda=self.cuda, global_scorer=self.global_scorer,
+                     pad=self.pad, eos=self.eos, bos=self.sos, min_length=self.min_length,
+                     stepwise_penalty=self.stepwise_penalty) for __ in range(batch_size)]
         # Help functions for working with beams and batches
 
         def var(a):
@@ -1175,3 +1169,239 @@ class PenaltyBuilder(object):
         Returns unmodified scores.
         """
         return logprobs
+    
+    
+class GlobalAttention(nn.Module):
+    """
+    Global attention takes a matrix and a query vector. It
+    then computes a parameterized convex combination of the matrix
+    based on the input query.
+
+    Constructs a unit mapping a query `q` of size `dim`
+    and a source matrix `H` of size `n x dim`, to an output
+    of size `dim`.
+
+    Args:
+       type(string): user which attention mechanism
+       dim (int): dimensionality of query and key
+       coverage (bool): use coverage term
+       attn_type (str): type of attention to use, options [dot,general,mlp]
+
+    """
+    def __init__(self, type, dim, coverage=False, attn_type="general"):
+        super(GlobalAttention, self).__init__()
+        self.type = type
+        self.dim = dim
+        self.attn_type = attn_type
+        self.linear_in = nn.Linear(dim, dim, bias=False)
+        # mlp wants it with bias
+        out_bias = self.attn_type == "mlp"
+        self.linear_out = nn.Linear(dim*2, dim, bias=out_bias)
+        self.sm = nn.Softmax(dim=1)
+        self.tanh = nn.Tanh()
+
+    def score(self, h_t, h_s):
+        """
+        Args:
+          h_t (`FloatTensor`): sequence of queries `[batch x tgt_len x dim]`
+          h_s (`FloatTensor`): sequence of sources `[batch x src_len x dim]`
+
+        Returns:
+          :obj:`FloatTensor`:
+           raw attention scores (unnormalized) for each src index
+          `[batch x tgt_len x src_len]`
+
+        """
+        # Check input sizes
+        src_batch, src_len, src_dim = h_s.size()
+        tgt_batch, tgt_len, tgt_dim = h_t.size()
+        h_t_ = h_t.view(tgt_batch * tgt_len, tgt_dim)
+        h_t_ = self.linear_in(h_t_)
+        h_t = h_t_.view(tgt_batch, tgt_len, tgt_dim)
+        h_s_ = h_s.transpose(1, 2)
+        # (batch, t_len, d) x (batch, d, s_len) --> (batch, t_len, s_len)
+        return torch.bmm(h_t, h_s_)
+
+    def forward(self, input, memory_bank, memory_lengths=None, coverage=None):
+        """
+
+        Args:
+
+          input (`FloatTensor`): query vectors `[batch x tgt_len x dim]`
+          memory_bank (`FloatTensor`): source vectors `[batch x src_len x dim]`
+          memory_lengths (`LongTensor`): the source context lengths `[batch]`
+          coverage (`FloatTensor`): None (not supported yet)
+
+        Returns:
+          (`FloatTensor`, `FloatTensor`):
+
+          * Computed vector `[tgt_len x batch x dim]`
+          * Attention distribtutions for each query
+             `[tgt_len x batch x src_len]`
+        """
+
+        # one step input
+
+        if input.dim() == 2:
+
+            one_step = True
+            input = input.unsqueeze(1)
+        else:
+            one_step = False
+
+        batch, sourceL, dim = memory_bank.size()
+        batch_, targetL, dim_ = input.size()
+        # print(type)
+        # print(memory_bank.size())
+        # print(input.size())
+        # compute attention scores, as in Luong et al.
+        align = self.score(input, memory_bank)
+        if self.type == 'GlobalAttention':
+            if memory_lengths is not None:
+                mask = sequence_mask(memory_lengths)
+                mask = mask.unsqueeze(1)  # Make it broadcastable.
+                align.data.masked_fill_(~mask, -float('inf'))
+            # Softmax to normalize attention weights
+            align_vectors = self.sm(align.view(batch*targetL, sourceL))
+            align_vectors = align_vectors.view(batch, targetL, sourceL)
+            # each context vector c_t is the weighted average
+            # over all the source hidden states
+            c = torch.bmm(align_vectors, memory_bank)
+            # concatenate
+            concat_c = torch.cat([c, input], 2).view(batch*targetL, dim*2)
+            attn_h = self.linear_out(concat_c).view(batch, targetL, dim)
+            if self.attn_type in ["general", "dot"]:
+                attn_h = self.tanh(attn_h)
+            if one_step:
+                attn_h = attn_h.squeeze(1)
+                align_vectors = align_vectors.squeeze(1)
+            else:
+                attn_h = attn_h.transpose(0, 1).contiguous()
+                align_vectors = align_vectors.transpose(0, 1).contiguous()
+            return attn_h, align_vectors
+        elif self.type == 'PointerAttention':
+            align_vectors = self.sm(align.view(batch * targetL, sourceL))
+            align_vectors = align_vectors + 1e-8
+            align_vectors = torch.log(align_vectors)
+            align_vectors = align_vectors.view(batch, targetL, sourceL)
+            align_vectors = align_vectors.transpose(0, 1).contiguous()
+            return align_vectors
+
+class GlobalSelfAttention(nn.Module):
+    """
+    Args:
+       dim (int): dimensionality of query and key
+       coverage (bool): use coverage term
+       attn_type (str): type of attention to use, options [dot,general,mlp]
+    """
+    def __init__(self, dim, coverage=False, attn_type="dot", attn_hidden=0):
+        super(GlobalSelfAttention, self).__init__()
+        self.dim = dim
+        self.attn_type = attn_type
+        self.attn_hidden = attn_hidden
+        if attn_hidden > 0:  # 64
+            self.transform_in = nn.Sequential(nn.Linear(dim, attn_hidden), nn.ELU(0.1))
+        if self.attn_type == "general":
+            d = attn_hidden if attn_hidden > 0 else dim
+            self.linear_in = nn.Linear(d, d, bias=False)
+        # mlp wants it with bias
+        out_bias = self.attn_type in ("mlp", "fine")
+        self.linear_out = nn.Linear(dim*2, dim, bias=out_bias)
+        self.sm = nn.Softmax(dim=2)
+        self.tanh = nn.Tanh()
+        self.dropout = nn.Dropout(p=0.3)
+
+    def score(self, h_t, h_s):
+        """
+        Args:
+          h_t (`FloatTensor`): sequence of queries `[batch x tgt_len x dim]`
+          h_s (`FloatTensor`): sequence of sources `[batch x src_len x dim]`
+
+        Returns:
+          :obj:`FloatTensor`:
+           raw attention scores (unnormalized) for each src index
+          `[batch x tgt_len x src_len]`
+
+        """
+
+        # Check input sizes
+        src_batch, src_len, src_dim = h_s.size()
+        tgt_batch, tgt_len, tgt_dim = h_t.size()
+        if self.attn_type in ["general", "dot"]:
+            if self.attn_hidden >  0:
+                h_t = self.transform_in(h_t)    # dim->attn_hidden
+                h_s = self.transform_in(h_s)
+            if self.attn_type == "general":
+                h_t = self.linear_in(h_t)
+            h_s_ = h_s.transpose(1, 2)
+            # (batch, t_len, d) x (batch, d, s_len) --> (batch, t_len, s_len)
+            return torch.bmm(h_t, h_s_)
+
+    def forward(self, input, memory_bank, memory_lengths=None, coverage=None):
+        """
+        Args:
+          input (`FloatTensor`): query vectors `[batch x tgt_len x dim]`
+          memory_bank (`FloatTensor`): source vectors `[batch x src_len x dim]`
+          memory_lengths (`LongTensor`): the source context lengths `[batch]`
+          coverage (`FloatTensor`): None (not supported yet)
+
+        Returns:
+          (`FloatTensor`, `FloatTensor`):
+
+          * Computed vector `[tgt_len x batch x dim]`
+          * Attention distribtutions for each query
+             `[tgt_len x batch x src_len]`
+        """
+        # one step input
+        if input.dim() == 2:
+            one_step = True
+            input = input.unsqueeze(1)
+        else:
+            one_step = False
+
+        batch, sourceL, dim = memory_bank.size()
+        batch_, targetL, dim_ = input.size()
+        align = self.score(input, memory_bank)    # `[batch x tgt_len x src_len]`
+        mask = sequence_mask(memory_lengths)
+        mask = mask.unsqueeze(1)  # Make it broadcastable.
+        # mask the time step of self
+        mask = mask.repeat(1, sourceL, 1)
+        mask_self_index = list(range(sourceL))
+        mask[:, mask_self_index, mask_self_index] = 0
+        if self.attn_type == "fine":
+            mask = mask.unsqueeze(3)
+        # print(mask)
+        align.data.masked_fill_(~mask, -float('inf'))
+        # Softmax to normalize attention weights
+        align_vectors = self.sm(align)
+        # each context vector c_t is the weighted average
+        # over all the source hidden states
+        if self.attn_type == "fine":
+            c = memory_bank.unsqueeze(1).mul(
+                align_vectors).sum(dim=2, keepdim=False)
+        else:
+            c = torch.bmm(align_vectors, memory_bank)
+        # concatenate
+        concat_c = torch.cat([c, input], 2)
+        attn_h = self.linear_out(concat_c)
+        if self.attn_type in ["general", "dot"]:
+            attn_h = torch.sigmoid(attn_h).mul(input)
+        if one_step:
+            attn_h = attn_h.squeeze(1)
+            align_vectors = align_vectors.squeeze(1)
+        else:
+            attn_h = attn_h.transpose(0, 1).contiguous()
+            align_vectors = align_vectors.transpose(0, 1).contiguous()
+        return attn_h, align_vectors
+
+def sequence_mask(lengths, max_len=None):
+    """
+    Creates a boolean mask from sequence lengths.
+    """
+    batch_size = lengths.numel()
+    max_len = max_len or lengths.max()
+    return (torch.arange(0, max_len)
+            .type_as(lengths)
+            .repeat(batch_size, 1)
+            .lt(lengths.unsqueeze(1)))
+

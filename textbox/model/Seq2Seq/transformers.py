44992b4525a6f9d055155d431315bf618a85e8a1
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-22 17:24:27 +0800

    integrate t5 bart bert2bert gpt2seq in transformers.py

--- textbox/model/Seq2Seq/transformers.py
+++ textbox/model/Seq2Seq/transformers.py
@@ -7,15 +7,28 @@ import torch.functional as F
 from textbox.model.abstract_generator import Seq2SeqGenerator
 
 from transformers import (
-    T5Config, T5Tokenizer, T5ForConditionalGeneration,
-    BartConfig, BartTokenizer, BartForConditionalGeneration
+    T5Tokenizer, T5ForConditionalGeneration,
+    BartTokenizer, BartForConditionalGeneration,
+    GPT2Tokenizer, GPT2LMHeadModel,
+    BertTokenizer, BertGenerationEncoder, BertGenerationDecoder, EncoderDecoderModel
 )
 
 MODEL_CLASSES = {
-    'T5': {
-        'config': T5Config,
+    't5': {
         'tokenizer': T5Tokenizer,
         'model': T5ForConditionalGeneration
+    },
+    'bart': {
+        'tokenizer': BartTokenizer,
+        'model': BartForConditionalGeneration
+    },
+    'gpt2seq': {
+        'tokenizer': GPT2Tokenizer,
+        'model': GPT2LMHeadModel
+    },
+    'bert2bert': {
+        'tokenizer': BertTokenizer,
+        'model': EncoderDecoderModel
     },
 
 }
@@ -25,16 +38,19 @@ class Transformers(Seq2SeqGenerator):
     def __init__(self, config, dataset):
         super(Transformers, self).__init__(config, dataset)
 
-        self.pretrained_model_path = config['pretrained_model_path']
-        config_class = MODEL_CLASSES[self.config['model']]['config']
-        tokenizer_class = MODEL_CLASSES[self.config['model']]['tokenizer']
-        model_class = MODEL_CLASSES[self.config['model']]['model']
+        self.model_name_or_path = config['pretrained_model_path']
+        self.model_name = config['model'].lower()
 
-        self.configuration = config_class.from_pretrained(self.pretrained_model_path)
-        self.tokenizer = tokenizer_class.from_pretrained(self.pretrained_model_path)
-        self.model = model_class.from_pretrained(self.pretrained_model_path, config=self.configuration)
+        tokenizer_class = MODEL_CLASSES[self.model_name]['tokenizer']
+        model_class = MODEL_CLASSES[self.model_name]['model']
 
-        self.model_type = self.configuration.model_type
+        if self.model_name == 'bert2bert':
+            self.model = model_class.from_encoder_decoder_pretrained(self.model_name_or_path, self.model_name_or_path)
+        else:
+            self.model = model_class.from_pretrained(self.model_name_or_path)
+
+        self.configuration = self.model.config
+        self.tokenizer = tokenizer_class.from_pretrained(self.model_name_or_path)
 
         self._check_params()
 
@@ -43,32 +59,34 @@ class Transformers(Seq2SeqGenerator):
         self.task_prefix_ids = self.tokenizer.encode(self.task_prefix, add_special_tokens=False)
         self.task_infix_ids = self.tokenizer.encode(self.task_infix, add_special_tokens=False)
 
-    def generate(self, batch_data, eval_data):
-        source_text = batch_data['source_text']
+    # def generate(self, batch_data, eval_data):
+    #     source_text = batch_data['source_text']
+    #
+    #     generate_corpus = []
+    #
+    #     for src in source_text:
+    #         src_ids = self.tokenizer.encode(src, add_special_tokens=False)
+    #
+    #         if self.model_type == 'gpt2':
+    #             input_id = src_ids + self.task_infix_ids
+    #             max_length = self.max_source_length + len(self.task_infix_ids) + self.max_target_length
+    #         else:
+    #             input_id = self.task_prefix_ids + src_ids
+    #             max_length = self.max_target_length
+    #
+    #         input_id = torch.tensor(input_id, dtype=torch.long, device=self.device).unsqueeze(1, -1)
+    #
+    #         outputs = self.model.generate(input_id, num_beams=5, max_length=max_length, early_stopping=True)
+    #
+    #         generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True).split()
+    #
+    #         generate_corpus.append(generated_text)
+    #
+    #     return generate_corpus
 
-        generate_corpus = []
-
-        for src in source_text:
-            src_ids = self.tokenizer.encode(src, add_special_tokens=False)
-
-            if self.model_type == 'gpt2':
-                input_id = src_ids + self.task_infix_ids
-                max_length = self.max_source_length + len(self.task_infix_ids) + self.max_target_length
-            else:
-                input_id = self.task_prefix_ids + src_ids
-                max_length = self.max_target_length
-
-            input_id = torch.tensor(input_id, dtype=torch.long, device=self.device).unsqueeze(1, -1)
-
-            outputs = self.model.generate(input_id, num_beams=5, max_length=max_length, early_stopping=True)
-
-            generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True).split()
-
-            generate_corpus.append(generated_text)
-
-        return generate_corpus
-
-    def _convert_text_to_tensors(self, source_text, target_text):
+    def _generate_default_inputs(self, corpus):
+        source_text = corpus['source_text']
+        target_text = corpus['target_text']
         input_ids = []
         labels = []
         attn_masks = []
@@ -77,10 +95,11 @@ class Transformers(Seq2SeqGenerator):
             src_ids = self.tokenizer.encode(src, add_special_tokens=False)
             tgt_ids = self.tokenizer.encode(tgt, add_special_tokens=False)
 
-            if self.model_type == 'gpt2':
-                src_ids = src_ids[:self.source_max_length - len(self.task_infix_ids)]
+            if self.model_name == 'gpt2seq':
+                src_ids = src_ids[:self.source_max_length - len(self.task_prefix_ids) - len(self.task_infix_ids)]
                 tgt_ids = tgt_ids[:self.target_max_length - len([self.tokenizer.eos_token_id])]
-                input_id = src_ids + self.task_infix_ids + tgt_ids + [self.tokenizer.eos_token_id]
+                input_id = self.task_prefix_ids + src_ids + self.task_infix_ids + tgt_ids + [
+                    self.tokenizer.eos_token_id]
                 label = copy.deepcopy(input_id)
 
                 padding_length = self.source_max_length + self.target_max_length - len(input_id)
@@ -118,23 +137,27 @@ class Transformers(Seq2SeqGenerator):
         labels = torch.tensor(labels, dtype=torch.long).to(self.device)
         attn_masks = torch.tensor(attn_masks, dtype=torch.long).to(self.device)
 
-        return input_ids, labels, attn_masks
+        inputs = {'input_ids': input_ids, 'attention_mask': attn_masks, 'labels': labels}
+        if self.model_name == 'bert2bert':
+            inputs['decoder_input_ids'] = labels.clone().detach()
+
+        return inputs
 
     def forward(self, corpus, epoch_idx=-1):
-        source_text = corpus['source_text']
-        target_text = corpus['target_text']
-
-        input_ids, labels, attn_masks = self._convert_text_to_tensors(source_text, target_text)
-
-        outputs = self.model(input_ids, attention_mask=attn_masks, labels=labels)
-
+        inputs = self._generate_default_inputs(corpus)
+        outputs = self.model(**inputs)
         return outputs.loss
 
     def _check_params(self):
-        model_type = self.configuration.model_type
-
-        if model_type == 'gpt2':
+        if self.model_name == 'gpt2seq':
             self.tokenizer.add_prefix_space = True
             self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
             self.configuration.pad_token_id = self.tokenizer.pad_token_id
             self.model.resize_token_embeddings(len(self.tokenizer))
+        elif self.model_name == 'bart':
+            self.configuration.forced_bos_token_id = self.tokenizer.bos_token_id
+
+        elif self.model_name == 'bert2bert':
+            self.configuration.decoder_start_token_id = self.tokenizer.cls_token_id
+            self.configuration.eos_token_id = self.tokenizer.sep_token_id
+            self.tokenizer.eos_token = self.tokenizer.sep_token

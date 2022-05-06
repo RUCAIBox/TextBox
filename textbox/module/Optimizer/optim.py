eef81b963b216dbd8abc7985b352a76e1aa3ea4a
huyiwen <1020030101@qq.com>
2022-05-06 16:51:12 +0800

    add schedule config of optimizer

--- textbox/module/Optimizer/optim.py
+++ textbox/module/Optimizer/optim.py
@@ -2,18 +2,23 @@
 # @Author : Junyi Li
 # @Email  : lijunyi@ruc.edu.cn
 
+# UPDATE:
+# @Time   : 2022/05/06
+# @Author : Hu Yiwen
+# @Email  : huyiwen@ruc.edu.cn
+
 r"""
 Optimizer
 #####################
 """
 
 import numpy as np
-from abc import ABC, abstractmethod
 
 from torch.optim import Optimizer as torch_optim
 
 
-class AbstractOptim(ABC):
+class AbstractOptim:
+
     def __init__(self, base_optimizer: torch_optim, init_lr: float):
         self.optimizer = base_optimizer
         self.init_lr = init_lr
@@ -28,21 +33,20 @@ class AbstractOptim(ABC):
         self.n_steps += 1
         lr = self.lr
 
-        for param_group in self._optimizer.param_groups:
+        for param_group in self.optimizer.param_groups:
             param_group['lr'] = lr
 
     @property
-    @abstractmethod
     def lr(self):
         """Get learning rate for current step."""
-        pass
+        raise NotImplementedError
 
     def state_dict(self):
-        return self._optimizer.state_dict(), self.n_steps
+        return self.optimizer.state_dict(), self.n_steps
 
     def load_state_dict(self, state_dict: tuple):
         opt, self.n_steps = state_dict
-        self._optimizer.load_state_dict(opt)
+        self.optimizer.load_state_dict(opt)
 
     def __getattr__(self, item: str):
         """Pass method calls e.g. `zero_grad()`.
@@ -51,32 +55,25 @@ class AbstractOptim(ABC):
         return getattr(self.optimizer, item)
 
 
-class ScheduledOptim(AbstractOptim):
-    def __init__(self, base_optimizer: torch_optim, init_lr: float, d_model: int, n_warmup_steps: int):
-        super().__init__(base_optimizer, init_lr * d_model ** (-0.5))
-        self.warmup_k = n_warmup_steps ** 1.5
-
-    @property
-    def lr(self):
-        return self.init_lr * min(self.n_steps ** (-0.5), self.warmup_k * self.n_steps)
-
-
 class InverseSquareRootOptim(AbstractOptim):
+
     def __init__(self, base_optimizer: torch_optim, init_lr: float, max_lr: float, n_warmup_steps: int):
         super().__init__(base_optimizer, init_lr)
         self.n_warmup_steps = n_warmup_steps
+
         self.warmup_k = (max_lr - init_lr) / n_warmup_steps
-        self.decay_factor = max_lr * (n_warmup_steps ** 0.5)
+        self.decay_k = max_lr * (n_warmup_steps ** 0.5)
 
     @property
     def lr(self):
         if self.n_steps <= self.n_warmup_steps:
             return self.init_lr + self.warmup_k * self.n_steps
         else:
-            return self.decay_factor * self.n_steps ** -0.5
+            return self.decay_k * self.n_steps ** -0.5
 
 
 class CosineOptim(AbstractOptim):
+
     def __init__(self, base_optimizer: torch_optim, init_lr: float, max_lr: float, n_warmup_steps: int, max_steps: int):
         super().__init__(base_optimizer, init_lr)
         self.n_warmup_steps = n_warmup_steps
@@ -94,6 +91,7 @@ class CosineOptim(AbstractOptim):
 
 
 class LinearOptim(AbstractOptim):
+
     def __init__(self, base_optimizer: torch_optim, init_lr: float, max_lr: float, n_warmup_steps: int, max_steps: int):
         super().__init__(base_optimizer, init_lr)
         self.n_warmup_steps = n_warmup_steps
@@ -112,6 +110,7 @@ class LinearOptim(AbstractOptim):
 
 
 class ConstantOptim(AbstractOptim):
+
     def __init__(self, base_optimizer: torch_optim, init_lr: float, max_lr: float, n_warmup_steps: int):
         super().__init__(base_optimizer, init_lr)
         self.n_warmup_steps = n_warmup_steps

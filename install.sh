caf7442f1b382a9eb1ef294c807d8f4e535efac2
huyiwen <1020030101@qq.com>
2022-08-01 11:14:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,33 +3,49 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR" || exit
 
-conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+echo "
+████████╗███████╗██╗  ██╗████████╗██████╗  ██████╗ ██╗  ██╗
+╚══██╔══╝██╔════╝╚██╗██╔╝╚══██╔══╝██╔══██╗██╔═══██╗╚██╗██╔╝
+   ██║   █████╗   ╚███╔╝    ██║   ██████╔╝██║   ██║ ╚███╔╝
+   ██║   ██╔══╝   ██╔██╗    ██║   ██╔══██╗██║   ██║ ██╔██╗
+   ██║   ███████╗██╔╝ ██╗   ██║   ██████╔╝╚██████╔╝██╔╝ ██╗
+   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═════╝  ╚═════╝ ╚═╝  ╚═╝
+
+"
+
+echo "Installing torch ..."
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch > /dev/null
+
+echo "Installing requirements ..."
 pip install -r requirements.txt > /dev/null
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     if ! command -v gcc-11 &> /dev/null
     then
-        echo "Install gcc-11 first. (brew install gcc@11)"
+        echo "Please install gcc-11 first. (brew install gcc@11)"
         exit
     fi
-    pip install fast-bleu --install-option="--CC=$(which gcc-11)" --install-option="--CXX=$(which g++-11)"
+    pip install fast-bleu --install-option="--CC=$(which gcc-11)" --install-option="--CXX=$(which g++-11)" > /dev/null
 else
-    pip install fast-bleu
+    pip install fast-bleu > /dev/null
 fi
 
 pip install git+https://github.com/microsoft/fastseq.git > /dev/null
 
-cd transformers || exit
-pip install -e .
-cd ..
-
-pip install -U git+https://github.com/pltrdy/pyrouge
-git clone https://github.com/pltrdy/files2rouge.git 
+pip install -U git+https://github.com/pltrdy/pyrouge > /dev/null
+git clone https://github.com/pltrdy/files2rouge.git  > /dev/null
 cd files2rouge || exit
-python setup_rouge.py
+python setup_rouge.py > /dev/null
 python setup.py install > /dev/null
 
 pip uninstall py-rouge
 pip install rouge
+
+echo "Installing transformers (modified) ..."
+cd transformers || exit
+pip install -e .
+cd ..
+
+wandb login
 
 exit 0

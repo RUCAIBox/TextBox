2705d4b885e9b6f4d8fe79c37eaad59f0f5749f1
Steven_Tang1998 <1020139164@qq.com>
2022-08-23 22:41:28 +0800

    Improve W&B interface and email notification

--- textbox/utils/__init__.py
+++ textbox/utils/__init__.py
@@ -1,3 +1,3 @@
 from textbox.utils.logger import init_logger
-from textbox.utils.utils import get_local_time, ensure_dir, get_model, get_trainer, init_seed, serialized_save
+from textbox.utils.utils import get_local_time, ensure_dir, get_model, get_trainer, init_seed, serialized_save, safe_remove
 from textbox.utils.argument_list import general_parameters, training_parameters, evaluation_parameters

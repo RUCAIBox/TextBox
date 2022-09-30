e8ffa01ffa3bd7c43855d3179d39d339f94a4938
1190303125 <2424390036@qq.com>
2022-09-30 10:22:16 +0800

    modify rnn model and move some functions to abstract_model

--- textbox/model/abstract_model.py
+++ textbox/model/abstract_model.py
@@ -30,3 +30,33 @@ class AbstractModel(nn.Module):
         """
         params = sum(p.numel() for p in self.parameters() if p.requires_grad)
         return super().__str__() + '\nTrainable parameters: {}'.format(params)
+    
+    def forward(self, batch, epoch_idx=-1):
+        inputs = {
+            'input_ids': batch['source_ids'].to(self.device),
+            'attention_mask': batch['source_mask'].to(self.device),
+            'labels': batch['target_ids'].to(self.device)
+        }
+        outputs = self.model(**inputs)
+
+        if self.label_smoothing:
+            loss_fct = nn.CrossEntropyLoss(label_smoothing=self.label_smoothing)
+            vocab_size = outputs.logits.size()[-1]
+            return loss_fct(outputs.logits.view(-1, vocab_size), inputs['labels'].view(-1))
+        else:
+            return outputs.loss
+
+    def generate(self, batch, eval_data, accelerator=None):
+        inputs = {
+            'input_ids': batch['source_ids'].to(self.device),
+            'attention_mask': batch['source_mask'].to(self.device),
+        }
+        sample_outputs = accelerator.unwrap_model(self.model).generate(**inputs, **self.generation_kwargs)
+        sample_outputs = accelerator.pad_across_processes(sample_outputs, dim=1, pad_index=self.tokenizer.pad_token_id)
+        sample_outputs = accelerator.gather((sample_outputs))
+
+        decode_kwargs = {'skip_special_tokens': True, 'clean_up_tokenization_spaces': False}
+        generated_text = self.tokenizer.batch_decode(sample_outputs, **decode_kwargs)
+        generated_text = [g.strip() or 'NULL' for g in generated_text]
+        # return generated_text
+        return generated_text
--- textbox/model/rnn_models.py
+++ textbox/model/rnn_models.py
@@ -5,7 +5,6 @@ from typing import Optional, Tuple, Dict, Any
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import AutoModelForSeq2SeqLM
 from transformers.utils import ModelOutput
 from transformers.modeling_utils import PreTrainedModel
 from transformers.configuration_utils import PretrainedConfig
@@ -21,15 +20,11 @@ class RNNConfig(PretrainedConfig):
     def __init__(
         self,
         model_name,
-        model_class,
-        transformer,
-        device,
         input_size=768,
         hidden_size=768,
         vocab_size=50265,
         num_layers=1,
         bias=True,
-        batch_first=False,
         dropout=0.,
         encoder_bidirectional=True,
         pad_token_id=1,
@@ -37,42 +32,28 @@ class RNNConfig(PretrainedConfig):
         bos_token_id=0,
         num_labels=3,
         is_encoder_decoder=True,
-        decoder_start_token_id=2,
         forced_eos_token_id=2,
-        forced_bos_token_id=0,
         **kwargs
     ):
         self.vocab_size = vocab_size
         self.model_name = model_name
-        self.model_class = model_class
-        self.transformer = transformer
-        self.device_ = device
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.dropout = dropout
         self.num_layers = num_layers
         self.bias = bias
-        self.batch_first = batch_first
         self.encoder_bidirectional = encoder_bidirectional
-        self.decoder_start_token_id = decoder_start_token_id
         super().__init__(
             num_labels=num_labels,
             pad_token_id=pad_token_id,
             bos_token_id=bos_token_id,
             eos_token_id=eos_token_id,
             is_encoder_decoder=is_encoder_decoder,
-            decoder_start_token_id=decoder_start_token_id,
             forced_eos_token_id=forced_eos_token_id,
             **kwargs,
         )
 
         # ensure backward compatibility for BART CNN models
-        if self.forced_bos_token_id is None and kwargs.get("force_bos_token_to_be_generated", False):
-            self.forced_bos_token_id = self.bos_token_id
-            warnings.warn(
-                f"Please make sure the config includes `forced_bos_token_id={self.bos_token_id}` in future versions. "
-                "The config can simply be saved and uploaded again to be fixed."
-            )
 
 
 class RNNPretrainedModel(PreTrainedModel):
@@ -122,32 +103,37 @@ class RNNEncoder(RNNPretrainedModel):
 
     def __init__(self, config):
         super(RNNEncoder, self).__init__(config)
-        self.model_class = config.model_class
+        self.model_name = config.model_name
         self.input_size = config.input_size
         self.hidden_size = config.hidden_size
         self.num_layers = config.num_layers
         self.bias = config.bias
-        self.batch_first = config.batch_first
         self.dropout = config.dropout
         self.bidirectional = config.encoder_bidirectional
-        self.word_embeddings_encoder = config.transformer.model.encoder.embed_tokens
-        self.device_ = config.device_
+        self.vocab_size = config.vocab_size
+        self.word_embeddings_encoder = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=self.input_size)
         self.h_projection = nn.Linear(
             in_features=self.hidden_size + self.hidden_size * int(self.bidirectional),
             out_features=self.hidden_size,
-            bias=False
+            bias=self.bias
         )
         self.c_projection = nn.Linear(
             in_features=self.hidden_size + self.hidden_size * int(self.bidirectional),
             out_features=self.hidden_size,
-            bias=False
+            bias=self.bias
         )
+        if self.model_name == 'lstm':
+            self.model_class = nn.LSTM
+        elif self.model_name == 'gru':
+            self.model_class = nn.GRU
+        else:
+            self.model_class = nn.RNN
         self.encoder = self.model_class(
             input_size=self.input_size,
             hidden_size=self.hidden_size,
             num_layers=self.num_layers,
             bias=self.bias,
-            batch_first=self.batch_first,
+            batch_first=True,
             dropout=self.dropout,
             bidirectional=self.bidirectional
         )
@@ -159,7 +145,7 @@ class RNNEncoder(RNNPretrainedModel):
             num_bi = 2
         h = torch.zeros(num_bi * self.num_layers, batch_size, self.hidden_size)
         c = torch.zeros(num_bi * self.num_layers, batch_size, self.hidden_size)
-        return h.to(self.device_), c.to(self.device_)
+        return h.to(self.device), c.to(self.device)
 
     def forward(self, input_ids, attention_mask, output_attentions=False, output_hidden_states=False, return_dict=True):
         decoder_cells = None
@@ -167,7 +153,7 @@ class RNNEncoder(RNNPretrainedModel):
         X = torch.nn.utils.rnn.pack_padded_sequence(
             embeddings, attention_mask.sum(dim=1).tolist(), batch_first=True, enforce_sorted=False
         )
-        if self.model_class != nn.LSTM:
+        if self.model_name != 'lstm':
             # rnn,gru
             h_0, _ = self.init_hidden(attention_mask.shape[0])
             encoder_outputs, encoder_last_hidden_states = self.encoder(X, h_0)
@@ -182,7 +168,7 @@ class RNNEncoder(RNNPretrainedModel):
         ).transpose(0, 1)
         decoder_hidden_states = self.h_projection(decoder_hidden_states)
         decoder_hidden_states = [t for t in decoder_hidden_states]
-        if self.model_class == nn.LSTM:
+        if self.model_name == 'lstm':
             encoder_cells = encoder_last_cells.transpose(0, 1).reshape(attention_mask.shape[0], self.num_layers,
                                                                        -1).transpose(0, 1)
 
@@ -205,6 +191,8 @@ class RNNDecoder(RNNPretrainedModel):
         self.input_size = config.input_size
         self.hidden_size = config.hidden_size
         self.num_layers = config.num_layers
+        self.dropout = config.dropout
+        self.Dropout_layer = nn.Dropout(self.dropout)
         self.bias = config.bias
         if self.model_name.lower() == 'gru':
             self.model_class = nn.GRUCell
@@ -215,7 +203,9 @@ class RNNDecoder(RNNPretrainedModel):
         self.cells = []
         self.cells.append(
             self.model_class(
-                input_size=self.input_size + 2 * self.hidden_size, hidden_size=self.hidden_size, bias=self.bias
+                input_size=self.input_size + config.encoder_bidirectional * self.hidden_size + self.hidden_size,
+                hidden_size=self.hidden_size,
+                bias=self.bias
             )
         )
         for _ in range(self.num_layers - 1):
@@ -230,13 +220,13 @@ class RNNDecoder(RNNPretrainedModel):
         if self.model_name == 'lstm':
             for i in range(len(self.cells)):
                 (new_hidden, new_cell) = self.cells[i](X, (hidden[i], cells[i]))
-                X = new_hidden
+                X = self.Dropout_layer(new_hidden)
                 new_hiddens.append(new_hidden)
                 new_cells.append(new_cell)
         else:
             for i in range(len(self.cells)):
                 new_hidden = self.cells[i](X, hidden[i])
-                X = new_hidden
+                X = self.Dropout_layer(new_hidden)
                 new_hiddens.append(new_hidden)
         return new_hidden, new_hiddens, new_cells
 
@@ -247,24 +237,19 @@ class RNNSeq2Seq(RNNPretrainedModel):
         super(RNNSeq2Seq, self).__init__(config)
         self.config = config
         self.model_name = config.model_name
-        self.model_class = config.model_class
         self.input_size = config.input_size
         self.hidden_size = config.hidden_size
         self.vocab_size = config.vocab_size
         self.num_layers = config.num_layers
         self.bias = config.bias
-        self.device_ = config.device_
-        self.batch_first = config.batch_first
         self.dropout = config.dropout
         self.encoder_bidirectional = config.encoder_bidirectional
         self.pad_token_id = config.pad_token_id
         self.eos_token_id = config.eos_token_id
         self.bos_token_id = config.bos_token_id
-        self.transformer = config.transformer
 
-        self.word_embeddings_decoder = self.transformer.model.decoder.embed_tokens
-        self.lm_head = self.transformer.lm_head
-        self.final_logits_bias = self.transformer.final_logits_bias
+        self.word_embeddings_decoder = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=self.input_size)
+        self.lm_head = nn.Linear(in_features=self.hidden_size, out_features=self.vocab_size)
 
         self.loss_fct = CrossEntropyLoss(ignore_index=self.pad_token_id, reduction='mean')
         self.main_input_name = "input_ids"
@@ -274,7 +259,7 @@ class RNNSeq2Seq(RNNPretrainedModel):
         self.att_projection = nn.Linear(
             in_features=self.hidden_size + self.hidden_size * int(self.encoder_bidirectional),
             out_features=self.hidden_size,
-            bias=False
+            bias=self.bias
         )
         self.output_projection = nn.Linear(
             in_features=self.hidden_size * 2 + self.hidden_size * int(self.encoder_bidirectional),
@@ -295,10 +280,6 @@ class RNNSeq2Seq(RNNPretrainedModel):
             torch.arange(input_ids.shape[0]).view(-1, 1).repeat(1, expand_size).view(-1).to(input_ids.device)
         )
         input_ids = input_ids.index_select(0, expanded_return_idx)
-
-        if "token_type_ids" in model_kwargs:
-            token_type_ids = model_kwargs["token_type_ids"]
-            model_kwargs["token_type_ids"] = token_type_ids.index_select(0, expanded_return_idx)
 
         if attention_mask is not None:
             model_kwargs["attention_mask"] = attention_mask.index_select(0, expanded_return_idx)
@@ -339,11 +320,6 @@ class RNNSeq2Seq(RNNPretrainedModel):
         else:
             model_kwargs["past"] = None
 
-        # update token_type_ids with last value
-        if "token_type_ids" in model_kwargs:
-            token_type_ids = model_kwargs["token_type_ids"]
-            model_kwargs["token_type_ids"] = torch.cat([token_type_ids, token_type_ids[:, -1].unsqueeze(-1)], dim=-1)
-
         # update attention mask
         if not is_encoder_decoder:
             if "attention_mask" in model_kwargs:
@@ -382,14 +358,12 @@ class RNNSeq2Seq(RNNPretrainedModel):
         a_t = torch.bmm(alpha_t.unsqueeze(1), output_encoder).squeeze(1)
         return a_t
 
-    def shift_tokens_right(self, input_ids: torch.Tensor, pad_token_id: int, decoder_start_token_id: int):
+    def shift_tokens_right(self, input_ids: torch.Tensor, pad_token_id: int):
         """
         Shift input ids one token to the right.
         """
-        shifted_input_ids = input_ids.new_zeros(input_ids.shape)
-        shifted_input_ids[:, 1:] = input_ids[:, :-1].clone()
-        shifted_input_ids[:, 0] = decoder_start_token_id
-        shifted_input_ids = shifted_input_ids[:, 1:]
+        shifted_input_ids = input_ids.new_zeros((input_ids.shape[0], input_ids.shape[1] - 1))
+        shifted_input_ids = input_ids[:, :-1].clone()
 
         if pad_token_id is None:
             raise ValueError("self.model.config.pad_token_id has to be defined.")
@@ -411,7 +385,7 @@ class RNNSeq2Seq(RNNPretrainedModel):
     ):
         if decoder_input_ids is None and labels is not None:
             # decoder_input_ids generate if None
-            decoder_input_ids = self.shift_tokens_right(labels, self.pad_token_id, self.bos_token_id)
+            decoder_input_ids = self.shift_tokens_right(labels, self.pad_token_id)
 
         decoder_cells = None
         if self.model_name == 'lstm':
@@ -440,11 +414,11 @@ class RNNSeq2Seq(RNNPretrainedModel):
                     output_decoder = self.output_projection(torch.cat((a_t, decoder_hidden_state_last_layer), dim=-1))
                     decoder_outputs.append(output_decoder)
                 decoder_outputs = torch.stack(decoder_outputs).transpose(0, 1)
-                logits = self.lm_head(decoder_outputs) + self.final_logits_bias.to(self.device_)
+                decoder_outputs = torch.cat((Y[0].unsqueeze(1), decoder_outputs), dim=1)
+                logits = self.lm_head(decoder_outputs)
                 shift_logits = logits.contiguous()
                 labels.masked_fill_(labels == -100, self.pad_token_id)
-                shift_labels = labels[:, 1:].contiguous()
-                masked_lm_loss = self.loss_fct(shift_logits.view(-1, self.vocab_size), shift_labels.view(-1))
+                masked_lm_loss = self.loss_fct(shift_logits.view(-1, self.vocab_size), labels.view(-1))
             else:
                 a_t = encoder_outputs.a_t
                 if a_t is None:
@@ -456,7 +430,7 @@ class RNNSeq2Seq(RNNPretrainedModel):
                 )
                 a_t = self.attention(last_hidden_state, decoder_hidden_state_last_layer, attention_mask)
                 output_decoder = self.output_projection(torch.cat((a_t, decoder_hidden_state_last_layer), dim=-1))
-                logits = self.lm_head(output_decoder.unsqueeze(1)) + self.final_logits_bias.to(self.device_)
+                logits = self.lm_head(output_decoder.unsqueeze(1))
         else:
             # gru, rnn
             masked_lm_loss = None
@@ -480,11 +454,11 @@ class RNNSeq2Seq(RNNPretrainedModel):
                     output_decoder = self.output_projection(torch.cat((a_t, decoder_hidden_state_last_layer), dim=-1))
                     decoder_outputs.append(output_decoder)
                 decoder_outputs = torch.stack(decoder_outputs).transpose(0, 1)
-                logits = self.lm_head(decoder_outputs) + self.final_logits_bias.to(self.device_)
+                decoder_outputs = torch.cat((Y[0].unsqueeze(1), decoder_outputs), dim=1)
+                logits = self.lm_head(decoder_outputs)
                 shift_logits = logits.contiguous()
                 labels.masked_fill_(labels == -100, self.pad_token_id)
-                shift_labels = labels[:, 1:].contiguous()
-                masked_lm_loss = self.loss_fct(shift_logits.view(-1, self.vocab_size), shift_labels.view(-1))
+                masked_lm_loss = self.loss_fct(shift_logits.view(-1, self.vocab_size), labels.view(-1))
             else:
                 a_t = encoder_outputs.a_t
                 if a_t is None:
@@ -496,7 +470,8 @@ class RNNSeq2Seq(RNNPretrainedModel):
                 )
                 a_t = self.attention(last_hidden_state, decoder_hidden_state_last_layer, attention_mask)
                 output_decoder = self.output_projection(torch.cat((a_t, decoder_hidden_state_last_layer), dim=-1))
-                logits = self.lm_head(output_decoder.unsqueeze(1)) + self.final_logits_bias.to(self.device_)
+                logits = self.lm_head(output_decoder.unsqueeze(1))
+
         outputs = RNNOutput(
             logits=logits,
             loss=masked_lm_loss,
@@ -518,35 +493,18 @@ class RNN_Models(AbstractModel):
         self.source_max_length = config['src_len']
         self.target_max_length = config['tgt_len']
         # initialize model
-        if self.model_name == 'rnn':
-            self.model_class = nn.RNN
-        elif self.model_name == 'gru':
-            self.model_class = nn.GRU
-        else:
-            self.model_class = nn.LSTM
-        params_list = (list(inspect.signature(self.model_class.__init__).parameters.keys()))
-        config_dict = {key: val for key, val in config.final_config_dict.items() if key in params_list}
-
-        self.vocab_path = config['vocab_path']
-        # load the vocabulary of the transformer
-        self.transformer = AutoModelForSeq2SeqLM.from_pretrained(self.vocab_path)
         self.model_config = RNNConfig(
             model_name=self.model_name,
-            model_class=self.model_class,
-            transformer=self.transformer,
             input_size=config['input_size'],
             hidden_size=config['hidden_size'],
             vocab_size=config['vocab_size'],
             num_layers=config['num_layers'],
-            device=self.device,
             bias=config['bias'],
             dropout=config['dropout'],
             encoder_bidirectional=config['encoder_bidirectional'],
             pad_token_id=config['pad_token_id'],
             eos_token_id=config['eos_token_id'],
             bos_token_id=config['bos_token_id'],
-            batch_first=config['batch_first'],
-            decoder_start_token_id=config['decoder_start_token_id']
         )
         self.model = RNNSeq2Seq(self.model_config)
 
@@ -554,35 +512,6 @@ class RNN_Models(AbstractModel):
         # generation settings
         self.generation_kwargs = {}
         self.generation_kwargs['max_length'] = self.target_max_length
-        self.generation_kwargs['decoder_start_token_id'] = self.model_config.decoder_start_token_id
         self.generation_kwargs.update(config['generation_kwargs'] or {})
-
-    def forward(self, batch, epoch_idx=-1):
-        inputs = {
-            'input_ids': batch['source_ids'].to(self.device),
-            'attention_mask': batch['source_mask'].to(self.device),
-            'labels': batch['target_ids'].to(self.device)
-        }
-        outputs = self.model(**inputs)
-
-        if self.label_smoothing:
-            loss_fct = nn.CrossEntropyLoss(label_smoothing=self.label_smoothing)
-            vocab_size = outputs.logits.size()[-1]
-            return loss_fct(outputs.logits.view(-1, vocab_size), inputs['labels'][:, 1:].contiguous().view(-1))
-        else:
-            return outputs.loss
-
-    def generate(self, batch, eval_data, accelerator=None):
-        inputs = {
-            'input_ids': batch['source_ids'].to(self.device),
-            'attention_mask': batch['source_mask'].to(self.device),
-        }
-        sample_outputs = accelerator.unwrap_model(self.model).generate(**inputs, **self.generation_kwargs)
-        sample_outputs = accelerator.pad_across_processes(sample_outputs, dim=1, pad_index=self.tokenizer.pad_token_id)
-        sample_outputs = accelerator.gather((sample_outputs))
-
-        decode_kwargs = {'skip_special_tokens': True, 'clean_up_tokenization_spaces': False}
-        generated_text = self.tokenizer.batch_decode(sample_outputs, **decode_kwargs)
-        generated_text = [g.strip() or 'NULL' for g in generated_text]
-        # return generated_text
-        return generated_text
+        self.is_prompt_tuning = False
+        self.is_casual_model = True

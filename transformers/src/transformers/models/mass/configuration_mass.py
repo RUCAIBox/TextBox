23508bad7a61d7a58b2bba10b7194c7b70dc7c85
yhao-wang <yuhao_wang@njust.edu.cn>
2022-11-24 00:02:37 +0800

    modify model mass

--- transformers/src/transformers/models/mass/configuration_mass.py
+++ transformers/src/transformers/models/mass/configuration_mass.py
@@ -1,27 +1,5 @@
-# coding=utf-8
-# Copyright 2021 The Fairseq Authors and The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-""" BART model configuration"""
-import warnings
-from collections import OrderedDict
-from typing import Any, Mapping, Optional
-
-from ... import PreTrainedTokenizer
 from ...configuration_utils import PretrainedConfig
-from ...onnx import OnnxConfig, OnnxConfigWithPast, OnnxSeq2SeqConfigWithPast
-from ...onnx.utils import compute_effective_axis_dimension
-from ...utils import TensorType, is_torch_available, logging
+from ...utils import logging
 
 
 logger = logging.get_logger(__name__)

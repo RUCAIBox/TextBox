3a879cf709305ec45a43172fc7ba77f1dd7a5046
StevenTang1998 <1020139164@qq.com>
2022-12-09 10:25:23 +0800

    Improve BART pretraining

--- textbox/data/denoising_dataset.py
+++ textbox/data/denoising_dataset.py
@@ -17,20 +17,18 @@ class DenoisingCollate:
     BART Original hyperparams: https://github.com/facebookresearch/fairseq/issues/1899#issuecomment-1069429320
     """
 
-    mask_ratio: float = 0.3
-    poisson_lambda: float = 3.5
-    permutate_sentence_ratio: float = 1.0
-    pad_to_multiple_of: int = 16
-
     def __init__(self, config, tokenizer, set):
         self.config = config
         self.tokenizer = tokenizer
         self.set = set
-        self.is_casual_model = bool(config["model_name"] in CLM_MODELS)
+        self.mask_ratio = config['mask_ratio'] or 0.3
+        self.poisson_lambda = config['poisson_lambda'] or 3.5
+        self.permutate_sentence_ratio = config['permutate_sentence_ratio'] or 0.0
+        self.poisson_distribution = torch.distributions.Poisson(rate=self.poisson_lambda)
 
     @classmethod
     def get_type(cls) -> str:
-        return 'denoising(BART)'
+        return 'denoising (BART)'
 
     def __post_init__(self):
         if self.tokenizer.mask_token is None or self.tokenizer.eos_token is None:
@@ -43,46 +41,21 @@ class DenoisingCollate:
         """
         # Handle dict or lists with proper padding and conversion to tensor.
         batch = {}
-        source_text = []
         source_ids = []
-        source_mask = []
-        source_length = []
-        target_text = []
-        source_padding_side = ("left" if self.set == "test" and self.is_casual_model else self.tokenizer.padding_side)
 
         for sample in samples:
-            source_text.append(sample["source_text"])
-            src_id = (
-                torch.cat([sample["source_ids"], sample["target_ids"]])
-                if self.set != "test" and self.is_casual_model else sample["source_ids"]
-            )
-            source_ids.append(src_id)
-            source_mask.append(torch.ones(len(src_id), dtype=torch.long))
-            source_length.append(len(src_id))
-            target_text.append(sample["target_text"])
+            source_ids.append(sample["source_ids"])
+        source_ids = _pad_sequence(source_ids, self.tokenizer.pad_token_id)
+        batch["target_ids"] = _pad_sequence(source_ids, -100)
 
-        batch["source_text"] = source_text
-        batch["source_ids"] = _pad_sequence(source_ids, self.tokenizer.pad_token_id, source_padding_side)
-        batch["source_mask"] = _pad_sequence(source_mask, 0, source_padding_side)
-        batch["source_length"] = torch.tensor(source_length, dtype=torch.long)
-        batch["target_text"] = target_text
-        # If special token mask has been preprocessed,
-        # batch["decoder_input_ids"] = self.shift_tokens_right(batch["input_ids"])
-
-        do_permutate = False
-        batch["source_ids"] = batch["source_ids"].cpu().detach().numpy()
         if self.permutate_sentence_ratio > 0.0:
-            batch["source_ids"] = self.permutate_sentences(batch["source_ids"])
-            do_permutate = True
+            source_ids = self.permutate_sentences(source_ids)
 
-        if self.mask_ratio:
-            batch["source_ids"], batch["target_ids"] = self.add_whole_word_mask(batch["source_ids"], do_permutate)
+        if self.mask_ratio > 0.0:
+            source_ids = self.add_whole_word_mask(source_ids)
 
-        if isinstance(batch["source_ids"], np.ndarray):
-            batch["source_ids"] = torch.tensor(batch["source_ids"])
-
-        if isinstance(batch["target_ids"], np.ndarray):
-            batch["target_ids"] = torch.tensor(batch["target_ids"])
+        batch["source_ids"] = source_ids
+        batch["source_mask"] = source_ids.ne(self.tokenizer.pad_token_id)
 
         return batch
 
@@ -101,7 +74,7 @@ class DenoisingCollate:
             np.unique(sentence_ends[:, 0], return_index=True)[1][1:],
         )
 
-        for i in range(inputs.shape[0]):
+        for i in range(inputs.size(0)):
             substitutions = np.random.permutation(num_sentences[i])[:num_to_permute[i]]
 
             ordering = np.arange(0, num_sentences[i])
@@ -110,206 +83,77 @@ class DenoisingCollate:
             index = 0
             for j in ordering:
                 sentence = inputs[i, (sentence_ends[i][j - 1] if j > 0 else 0):sentence_ends[i][j]]
-                results[i, index:index + sentence.shape[0]] = sentence
-                index += sentence.shape[0]
+                results[i, index:index + sentence.size(0)] = sentence
+                index += sentence.size(0)
         return results
 
-    def add_whole_word_mask(self, inputs, do_permutate):
-        labels = inputs.copy()
-
+    def add_whole_word_mask(self, inputs):
+        bsz, seq_len = inputs.size()
         special_tokens_mask = [
-            self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+            self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in inputs.tolist()
         ]
-        special_tokens_mask = np.array(special_tokens_mask, dtype=bool)
-
+        special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
         # determine how many tokens we need to mask in total
-        is_token = ~(labels == self.tokenizer.pad_token_id) & ~special_tokens_mask
-        num_to_mask = int(math.ceil(is_token.astype(float).sum() * self.mask_ratio))
-        if num_to_mask == 0:
-            return torch.tensor(inputs), torch.tensor(labels)
+        num_to_mask = math.ceil((~special_tokens_mask).sum() * self.mask_ratio)
 
         # generate a sufficient number of span lengths
-        lengths = poisson(lam=self.poisson_lambda, size=(num_to_mask,))
-        while np.cumsum(lengths, 0)[-1] < num_to_mask:
-            lengths = np.concatenate([lengths, poisson(lam=self.poisson_lambda, size=(num_to_mask,))])
-
-        # remove all spans of length 0
-        # Note that BART inserts additional mask tokens where length == 0,
-        # which we do not implement for now as it adds additional complexity
-        lengths = lengths[lengths > 0]
+        lengths = self.poisson_distribution.sample(sample_shape=(num_to_mask,))
+        cum_length = torch.cumsum(lengths, 0)
+        while cum_length[-1] < num_to_mask:
+            lengths = torch.cat([lengths, self.poisson_distribution.sample(sample_shape=(num_to_mask,))])
+            cum_length = torch.cumsum(lengths, 0)
 
         # trim to about num_to_mask tokens
-        idx = np.argmin(np.abs(np.cumsum(lengths, 0) - num_to_mask)) + 1
-        lengths = lengths[:idx + 1]
+        idx = ((cum_length - num_to_mask) >= 0).nonzero()[0][0]
+        lengths[idx] = num_to_mask - (0 if idx == 0 else cum_length[idx - 1])
+        num_span = idx + 1
+        lengths = lengths[:num_span]
 
-        # select span start indices
-        token_indices = np.argwhere(is_token == 1)
-        span_starts = permutation(token_indices.shape[0])[:lengths.shape[0]]
-
-        # prepare mask
-        masked_indices = np.array(token_indices[span_starts])
-        mask = np.full_like(labels, fill_value=False)
-
-        # mask span start indices
-        for mi in masked_indices:
-            mask[tuple(mi)] = True
-        lengths -= 1
-
-        # fill up spans
-        max_index = labels.shape[1] - 1
-        remaining = (lengths > 0) & (masked_indices[:, 1] < max_index)
-        while np.any(remaining):
-            masked_indices[remaining, 1] += 1
-            for mi in masked_indices:
-                mask[tuple(mi)] = True
-            lengths -= 1
-            remaining = (lengths > 0) & (masked_indices[:, 1] < max_index)
-
-        # place the mask tokens
-        mask[np.where(special_tokens_mask)] = False
-        inputs[np.where(mask)] = self.tokenizer.mask_token_id
-
-        if not do_permutate:
-            labels[np.where(mask)] = -100
-        else:
-            labels[np.where(special_tokens_mask)] = -100
-
-        # remove mask tokens that are not starts of spans
-        to_remove = (mask == 1) & np.roll((mask == 1), 1, 1)
-        new_inputs = np.full_like(labels, fill_value=self.tokenizer.pad_token_id)
-
-        # splits = list(map(lambda x: x.reshape(-1),  np.split(inputs_copy, indices_or_sections=2, axis=0))
-        for i, example in enumerate(np.split(inputs, indices_or_sections=new_inputs.shape[0], axis=0)):
-            new_example = example[0][~to_remove[i]]
-            new_inputs[i, 0:new_example.shape[0]] = new_example
-
-        # batching now fixed
-        return new_inputs, labels
-
-
-class TextInfillingCollate:
-    # Original BART pretraining hyperparams: https://github.com/facebookresearch/fairseq/issues/1899#issuecomment-1069429320
-    mlm_probability: float = 0.3
-    poisson_lambda: float = 3.5
-    pad_to_multiple_of: Optional[int] = None
-
-    @classmethod
-    def get_type(cls) -> str:
-        return 'text infilling only'
-
-    def __init__(self, config, tokenizer, set):
-        self.config = config
-        self.tokenizer = tokenizer
-        self.set = set
-        self.is_casual_model = bool(config["model_name"] in CLM_MODELS)
-
-    def __post_init__(self):
-        if self.tokenizer.mask_token is None:
-            raise ValueError
-
-    def __call__(self, samples: List[Dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]:
-        # Handle dict or lists with proper padding and conversion to tensor.
-        # padded_encoder_input = _collate_batch(samples['source_ids'], self.tokenizer, pad_to_multiple_of=self.pad_to_multiple_of)
-        batch = {}
-        source_text = []
-        source_ids = []
-        source_mask = []
-        source_length = []
-        target_text = []
-        source_padding_side = ("left" if self.set == "test" and self.is_casual_model else self.tokenizer.padding_side)
-
-        for sample in samples:
-            source_text.append(sample["source_text"])
-            src_id = (
-                torch.cat([sample["source_ids"], sample["target_ids"]])
-                if self.set != "test" and self.is_casual_model else sample["source_ids"]
-            )
-            source_ids.append(src_id)
-            source_mask.append(torch.ones(len(src_id), dtype=torch.long))
-            source_length.append(len(src_id))
-            target_text.append(sample["target_text"])
-
-        batch["source_text"] = source_text
-        batch["source_ids"] = _pad_sequence(source_ids, self.tokenizer.pad_token_id, source_padding_side)
-        batch["source_mask"] = _pad_sequence(source_mask, 0, source_padding_side)
-        batch["source_length"] = torch.tensor(source_length, dtype=torch.long)
-        batch["target_text"] = target_text
-        # If special token mask has been preprocessed, pop it from the dict.
-        special_tokens_mask = batch.pop("special_tokens_mask", None)
-
-        batch["source_ids"], batch["target_ids"] = self.mask_tokens(
-            batch["source_ids"], special_tokens_mask=special_tokens_mask
-        )
-        return batch
-
-    def mask_tokens(self,
-                    inputs: torch.Tensor,
-                    special_tokens_mask: Optional[torch.Tensor] = None) -> Tuple[torch.Tensor, torch.Tensor]:
-        labels = inputs.clone()
-
-        if special_tokens_mask is None:
-            special_tokens_mask = [
-                self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
-            ]
-            special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
-        else:
-            special_tokens_mask = special_tokens_mask.bool()
-
-        # determine how many tokens we need to mask in total
-        is_token = ~(inputs == self.tokenizer.pad_token_id) & ~special_tokens_mask
-        num_to_mask = int(math.ceil(is_token.float().sum() * self.mlm_probability))
-
-        if num_to_mask == 0:
-            return inputs, labels
-
-        # generate a sufficient number of span lengths
-        poisson_distribution = torch.distributions.Poisson(rate=self.poisson_lambda)
-        lengths = poisson_distribution.sample(sample_shape=(num_to_mask,))
-        while torch.cumsum(lengths, 0)[-1] < num_to_mask:
-            lengths = torch.cat([lengths, poisson_distribution.sample(sample_shape=(num_to_mask,))])
-
-        # remove all spans of length 0
-        # Note that BART inserts additional mask tokens where length == 0,
-        # which we do not implement for now as it adds additional complexity
+        # handle 0-length mask (inserts) separately
         lengths = lengths[lengths > 0]
-
-        # trim to about num_to_mask tokens
-        idx = torch.argmin(torch.abs(torch.cumsum(lengths, 0) - num_to_mask)) + 1
-        lengths = lengths[:idx + 1]
+        num_inserts = num_span - lengths.size(0)
+        num_span -= num_inserts
 
         # select span start indices
-        token_indices = is_token.nonzero(as_tuple=False)
-        span_starts = torch.randperm(token_indices.shape[0])[:lengths.shape[0]]
+        token_indices = (~special_tokens_mask).nonzero()
+        rand_span = torch.randperm(token_indices.size(0))
+        span_starts = rand_span[:num_span]
 
-        # prepare mask
+        # prepare mask and mask span start indices
         masked_indices = token_indices[span_starts]
-        mask = torch.full_like(inputs, fill_value=False)
-
-        # mask span start indices
-        for mi in masked_indices:
-            mask[tuple(mi)] = True
+        mask = torch.zeros_like(inputs, dtype=torch.bool)
+        mask[tuple(masked_indices.t())] = True
         lengths -= 1
 
         # fill up spans
-        max_index = inputs.shape[1] - 1
-        remaining = (lengths > 0) & (masked_indices[:, 1] < max_index)
+        remaining = (lengths > 0) & (masked_indices[:, 1] < seq_len - 1)
         while torch.any(remaining):
             masked_indices[remaining, 1] += 1
-            for mi in masked_indices:
-                mask[tuple(mi)] = True
+            mask[tuple(masked_indices.t())] = True
             lengths -= 1
-            remaining = (lengths > 0) & (masked_indices[:, 1] < max_index)
+            remaining = (lengths > 0) & (masked_indices[:, 1] < seq_len - 1)
 
         # place the mask tokens
         mask[special_tokens_mask] = False
-        inputs[mask.bool()] = self.tokenizer.mask_token_id
-        labels[~mask.bool()] = -100
+        inputs[mask] = self.tokenizer.mask_token_id
 
         # remove mask tokens that are not starts of spans
-        to_remove = mask.bool() & mask.bool().roll(1, 1)
-        new_inputs = torch.full_like(inputs, fill_value=self.tokenizer.pad_token_id)
-        for i, example in enumerate(torch.split(inputs, split_size_or_sections=1, dim=0)):
-            new_example = example[0][~to_remove[i]]
-            new_inputs[i, 0:new_example.shape[0]] = new_example
-
-        return new_inputs, labels
+        to_remove = mask & mask.roll(1, 1) | inputs.eq(self.tokenizer.pad_token_id)
+        # calculate the number of inserted mask token per row
+        inserts_num = torch.bincount(token_indices[rand_span[:num_inserts]][:, 0], minlength=bsz)
+        new_inputs = []
+        for i, example in enumerate(inputs):
+            new_example = example[~to_remove[i]]
+            n = inserts_num[i]
+            if n:
+                new_num = n + new_example.size(0)
+                noise_mask = torch.zeros(new_num, dtype=torch.bool)
+                mask_indices = torch.randperm(new_num - 2)[:n] + 1
+                noise_mask[mask_indices] = 1
+                result = torch.LongTensor(new_num.item())
+                result[mask_indices] = self.tokenizer.mask_token_id
+                result[~noise_mask] = new_example
+                new_example = result
+            new_inputs.append(new_example)
+        new_inputs = _pad_sequence(new_inputs, self.tokenizer.pad_token_id)
+        return new_inputs
